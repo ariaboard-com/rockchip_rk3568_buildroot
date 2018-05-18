8bc74bed6afe67664ac398bb4dfa8a8d343ae9f5
xiaoyao <xiaoyao@rock-chips.com>
2018-05-18 09:52:30 +0800

    rkwifibt: support rtl8723ds/8189fs
    Change-Id: I4a34fddf977444855dd54e095e34edfc89058ac0
    Signed-off-by: xiaoyao <xiaoyao@rock-chips.com>

--- package/rockchip/rkwifibt/rkwifibt.mk
+++ package/rockchip/rkwifibt/rkwifibt.mk
@@ -35,6 +35,39 @@ CHIP_NAME = AW-NB197
 BT_FIRMWARE = BCM4343A1.hcd
 endif
 
+ifeq ($(BR2_PACKAGE_RKWIFIBT_RTL8723DS),y)
+CHIP_VENDOR = REALTEK
+CHIP_NAME = RTL8723DS
+BT_FIRMWARE = y
+endif
+
+ifeq ($(BR2_PACKAGE_RKWIFIBT_RTL8189FS),y)
+CHIP_VENDOR = REALTEK
+CHIP_NAME = RTL8189FS
+endif
+
+ifeq ($(CHIP_VENDOR), REALTEK)
+
+ifeq ($(CHIP_NAME), RTL8723DS)
+define RKWIFIBT_BUILD_CMDS
+	$(MAKE) -C $(@D)/realtek/rtk_hciattach/ CC=$(TARGET_CC)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(TOPDIR)/../kernel/ M=$(@D)/realtek/bluetooth_uart_driver ARCH=arm64 \
+			CROSS_COMPILE=$(TOPDIR)/../prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+endef
+
+define RKWIFIBT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/realtek/rtk_hciattach/rtk_hciattach $(TARGET_DIR)/usr/bin/rtk_hciattach
+	$(INSTALL) -D -m 0755 $(@D)/realtek/bluetooth_uart_driver/hci_uart.ko $(TARGET_DIR)/usr/lib/modules
+
+	mkdir -p $(TARGET_DIR)/lib/firmware/rtlbt/
+	$(INSTALL) -D -m 0755 $(@D)/realtek/$(CHIP_NAME)/* $(TARGET_DIR)/lib/firmware/rtlbt/
+	sed -i 's/BT_TTY_DEV/\/dev\/$(BT_TTY_DEV)/g' $(@D)/bt_load_rtk_firmware
+	$(INSTALL) -D -m 0755 $(@D)/bt_load_rtk_firmware $(TARGET_DIR)/usr/bin
+endef
+endif # RTL8723DS
+
+endif # CHIP_VENDOR
+
 ifeq ($(CHIP_VENDOR), BROADCOM)
 define RKWIFIBT_BUILD_CMDS
     $(TARGET_CC) -o $(@D)/brcm_tools/brcm_patchram_plus1 $(@D)/brcm_tools/brcm_patchram_plus1.c
@@ -51,9 +84,7 @@ define RKWIFIBT_INSTALL_TARGET_CMDS
 	@$(INSTALL) -D -m 0644 $(RKWIFIBT_MODULES_PATH)/rkwifi/bcmdhd/bcmdhd.ko $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and compile wifi modules first"
 	sed -i 's/MODULE_PATH/\/usr\/lib\/modules\/bcmdhd.ko/g' $(@D)/S42load_wifi_modules
 	$(INSTALL) -D -m 0755 $(@D)/S42load_wifi_modules $(TARGET_DIR)/etc/init.d
-	sed -i 's/BTFIRMWARE_PATH/\/system\/etc\/firmware\/$(BT_FIRMWARE)/g' $(@D)/rk_load_bt_firmware
-	sed -i 's/BT_TTY_DEV/\/dev\/$(BT_TTY_DEV)/g' $(@D)/rk_load_bt_firmware
-    $(INSTALL) -D -m 0755 $(@D)/rk_load_bt_firmware $(TARGET_DIR)/usr/bin
+	#sed -i 's/BTFIRMWARE_PATH/\/system\/etc\/firmware\/$(BT_FIRMWARE)/g' $(@D)/rk_load_bt_firmware
 	$(INSTALL) -D -m 0755 $(@D)/bin/* $(TARGET_DIR)/usr/bin/
 endef
 endif
