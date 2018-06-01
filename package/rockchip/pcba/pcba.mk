0a9b9d4d2af215bd46dc569db89321fa94f49d60
Eddie Cai <eddie.cai.linux@gmail.com>
2018-06-01 15:40:09 +0800

    package: rockchip: pcba: use source code from external
    Change-Id: I1a220f11e878842e818a8fcd785423a69e250369
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/pcba/pcba.mk
+++ package/rockchip/pcba/pcba.mk
@@ -1,6 +1,6 @@
 # Rockchip's pcba porting for Linux
 
-ifeq ($(BR2_PACKAGE_RK3036_ECHO),y)
+
 PCBA_SITE = $(TOPDIR)/../external/rk_pcba_test
 PCBA_SITE_METHOD = local
 
@@ -10,28 +10,3 @@ $(INSTALL) -D -m 0755 $(@D)/rk_pcba_test/* $(TARGET_DIR)/data
 endef
 
 $(eval $(cmake-package))
-endif
-
-ifeq ($(BR2_PACKAGE_RK3308),y)
-PCBA_SITE = $(TOPDIR)/../external/rk_pcba_test
-PCBA_SITE_METHOD = local
-
-define PCBA_INSTALL_INIT_SYSV
-$(INSTALL) -d -m 0755 $(TARGET_DIR)/data
-$(INSTALL) -D -m 0755 $(@D)/rk_pcba_test/* $(TARGET_DIR)/data
-endef
-
-$(eval $(cmake-package))
-endif
-
-ifeq ($(BR2_PACKAGE_RK3326),y)
-PCBA_SITE = $(TOPDIR)/../external/rk_pcba_test
-PCBA_SITE_METHOD = local
-
-define PCBA_INSTALL_INIT_SYSV
-$(INSTALL) -d -m 0755 $(TARGET_DIR)/data
-$(INSTALL) -D -m 0755 $(@D)/rk_pcba_test/* $(TARGET_DIR)/data
-endef
-
-$(eval $(cmake-package))
-endif
