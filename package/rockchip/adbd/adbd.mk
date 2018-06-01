0eab6808d960c68d0931c54615ccba40fcb6041d
Eddie Cai <eddie.cai.linux@gmail.com>
2018-06-01 15:24:40 +0800

    package: rockchip: copy S60adbd and usb_config when build adbd
    Change-Id: I2fc7eb9d43946508349683af496f421b6c2f1e26
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/adbd/adbd.mk
+++ package/rockchip/adbd/adbd.mk
@@ -1,24 +1,8 @@
 # Rockchip's adbd porting for Linux
 # Author : Cody Xie <cody.xie@rock-chips.com>
 
-ifeq ($(BR2_PACKAGE_RK3036_ECHO),y)
 ADBD_SITE = $(TOPDIR)/../external/adb
 ADBD_SITE_METHOD = local
-else ifeq ($(BR2_PACKAGE_RK3308),y)
-ADBD_SITE = $(TOPDIR)/../external/adb
-ADBD_SITE_METHOD = local
-else ifeq ($(BR2_PACKAGE_RK3326),y)
-ADBD_SITE = $(TOPDIR)/../external/adb
-ADBD_SITE_METHOD = local
-else ifeq ($(BR2_PACKAGE_RK312X),y)
-ADBD_SITE = $(TOPDIR)/../external/adb
-ADBD_SITE_METHOD = local
-else
-ADBD_SITE = $(call qstrip, ssh://git@10.10.10.78:2222/argus/externals/adb.git)
-ADBD_SITE_METHOD = git
-ADBD_SOURCE = adbd-${ADBD_VERSION}.tar.gz
-ADBD_FROM_GIT = y
-endif
 ADBD_VERSION = 0d8ec12
 
 ADBD_DEPENDENCIES += libcutils
@@ -31,13 +15,15 @@ endif
 
 ifeq ($(BR2_KERNEL_HEADERS_4_4),y)
 ADBD_INIT_SCRIPT=package/rockchip/adbd/S30adbd-configfs
+else ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4),y)
+ADBD_INIT_SCRIPT=package/rockchip/adbd/S60adbd
 else
 ADBD_INIT_SCRIPT=package/rockchip/adbd/S30adbd
 endif
 
 define ADBD_INSTALL_INIT_SYSV
 $(INSTALL) -D -m 0755 ${ADBD_INIT_SCRIPT} \
-		$(TARGET_DIR)/etc/init.d/S30adbd
+		$(TARGET_DIR)/etc/init.d/S60adbd
 $(INSTALL) -D -m 0755 package/rockchip/adbd/usb_config \
         $(TARGET_DIR)/usr/bin/usb_config
 endef
