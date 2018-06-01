cc9a6c5e745bfceb850e04a70320df902bd015a8
Eddie Cai <eddie.cai.linux@gmail.com>
2018-06-01 15:48:36 +0800

    package: rockchip: mpp: build with source code from external
    Change-Id: Ie1d9ef8dc003224aea9a8af60403fa3f713477d1
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/mpp/mpp.mk
+++ package/rockchip/mpp/mpp.mk
@@ -1,12 +1,7 @@
 # Rockchip's MPP(Multimedia Processing Platform)
-ifeq ($(BR2_PACKAGE_RK312X),y)
 MPP_SITE = $(TOPDIR)/../external/mpp
 MPP_VERSION = release
 MPP_SITE_METHOD = local
-else
-MPP_SITE = "git://github.com/rockchip-linux/mpp.git"
-MPP_VERSION = release
-endif
 
 MPP_CONF_OPTS = "-DRKPLATFORM=ON"
 MPP_CONF_DEPENDENCIES += libdrm
