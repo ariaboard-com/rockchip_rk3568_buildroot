53dd9e544ed85c2e9ebe8fa711b4bef3736f6243
Eddie Cai <eddie.cai.linux@gmail.com>
2018-06-22 18:24:36 +0800

    build: add script to merge defconfig
    Change-Id: Ifacf9bde0db7918ceebf71b7387db9f453ffdf9c
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

---
+++ build/mkconfig.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+BUILD_DIR=$(cd `dirname $0`; pwd)
+if [ -h $0 ]
+then
+        CMD=$(readlink $0)
+        BUILD_DIR=$(dirname $CMD)
+fi
+cd $BUILD_DIR/../../
+TOP_DIR=$(pwd)
+cd - > /dev/null
+CONIFG_DIR=$TOP_DIR/buildroot/configs
+CONFIG=$1
+LINE=$(head -n 1 $CONIFG_DIR/$CONFIG)
+ROCKCHIP=$(echo "$LINE" | cut -c1-8)
+TARGET_DIR=${CONFIG%_defconfig}
+mkdir -p $TOP_DIR/buildroot/out/$TARGET_DIR
+DST_CONFIG=$TOP_DIR/buildroot/output/$TARGET_DIR/.rockchipconfig
+SRC_CONFIG=$TOP_DIR/buildroot/configs/$CONFIG
+echo "dst:$DST_CONFIG"
+echo "src:$SRC_CONFIG"
+if [ $ROCKCHIP = rockchip ]
+then
+	rm $DST_CONFIG 2>/dev/null
+	for line in $(cat $SRC_CONFIG)
+	do
+		if [ -f $TOP_DIR/buildroot/configs/${line} ]
+		then 
+			echo "merge $TOP_DIR/buildroot/configs/${line}"
+			cat $TOP_DIR/buildroot/configs/${line} >> $DST_CONFIG
+		else
+			echo "${line}" >> $DST_CONFIG
+		fi
+	done
+else
+	cp $SRC_CONFIG $DST_CONFIG
+fi
+
