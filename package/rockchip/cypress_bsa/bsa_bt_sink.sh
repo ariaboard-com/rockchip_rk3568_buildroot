9c6f7001be33c085031c9c90adefc11aa022f743
Hans Yang <yhx@rock-chips.com>
2018-06-23 16:26:00 +0800

    Revert "cypress_bsa: bsa_bt_sink: redirect log to udisk"
    This reverts commit 8c4d427adadc6631afbba1eac59f25c37d88068c.
    
    Change-Id: Ibee510cc2af879f2a88080b71361dcaceac83ffc

--- package/rockchip/cypress_bsa/bsa_bt_sink.sh
+++ package/rockchip/cypress_bsa/bsa_bt_sink.sh
@@ -13,18 +13,17 @@ case "$1" in
 
     mkdir /data/bsa 2>/dev/null
     mkdir /data/bsa/config 2>/dev/null
-    mkdir /mnt/udisk/bsa
     cd /data/bsa/config
     echo "start broadcom bluetooth server bsa_sever"
     killall bsa_server
-    bsa_server -r 12 -b /mnt/udisk/bsa/btsnoop.log -p $hcd_file -d /dev/ttyS4 > /mnt/udisk/bsa/bsa_log &
+    bsa_server -r 12 -b /data/bsa/btsnoop.log -p $hcd_file -d /dev/ttyS4 > /data/bsa/bsa_log &
     sleep 2
 
     echo "start broadcom bluetooth app_manager"
-    app_manager -s > /mnt/udisk/bsa/app_mananger.log &
+    app_manager -s > /data/bsa/app_mananger.log &
 
     echo "start broadcom bluetooth app_avk"
-    app_avk -s > /mnt/udisk/bsa/app_avk.log &
+    app_avk -s > /data/bsa/app_avk.log &
     sleep 1
 
 
