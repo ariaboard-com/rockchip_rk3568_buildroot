8c21d6455b46f36addb26edf8ce10a9be5825c38
Nyx Zheng <zyh@rock-chips.com>
2018-07-02 15:03:13 +0800

    iflytekSDK: add support 32bit and 64bit libs in common
    Change-Id: I3c4de9441da1a327aed6593b75f638b84e907b66
    Signed-off-by: Nyx Zheng <zyh@rock-chips.com>

--- package/rockchip/iflytekSDK/iflytekSDK.mk
+++ package/rockchip/iflytekSDK/iflytekSDK.mk
@@ -7,17 +7,24 @@ ifeq ($(BR2_PACKAGE_IFLYTEKSDK), y)
 IFLYTEKSDK_VERSION:=1.0.0
 IFLYTEKSDK_SITE=$(TOPDIR)/../external/iflytekSDK
 IFLYTEKSDK_SITE_METHOD=local
-
+ifeq ($(BR2_aarch64),y)
 define IFLYTEKSDK_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE) CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
+    $(TARGET_MAKE_ENV) $(MAKE) CAE_PATH=libs_64bit CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
 endef
+define IFLYTEKSDK_INSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) CAE_PATH=libs_64bit -C $(@D) install
+endef
+else
+define IFLYTEKSDK_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) CAE_PATH=libs CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
+endef
+define IFLYTEKSDK_INSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) CAE_PATH=libs -C $(@D) install
+endef
+endif
 
 define IFLYTEKSDK_CLEAN_CMDS
     $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean
-endef
-
-define IFLYTEKSDK_INSTALL_TARGET_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
 endef
 
 define IFLYTEKSDK_UNINSTALL_TARGET_CMDS
