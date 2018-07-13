cf08e4b1590854182fac611cdd88af17fe90f088
Xing Zheng <zhengxing@rock-chips.com>
2018-07-13 18:33:46 +0800

    package: rockchip: add supports broadcom_bsa
    Change-Id: Iede902b6cab7713bb4d80092e4faf2ff45b97546
    Signed-off-by: Xing Zheng <zhengxing@rock-chips.com>

---
+++ package/rockchip/broadcom_bsa/broadcom_bsa.mk
@@ -0,0 +1,45 @@
+BROADCOM_BSA_SITE = $(TOPDIR)/../external/broadcom_bsa
+BROADCOM_BSA_SITE_METHOD = local
+
+BROADCOM_BSA_PATH = 3rdparty/embedded/bsa_examples/linux
+BROADCOM_BSA_LIBBSA = libbsa
+BROADCOM_BSA_APP = app_manager app_av app_avk app_ble app_dg \
+		   app_hl app_hs app_tm app_tm \
+		   app_hd app_hh
+
+BROADCOM_BSA_BUILD_TYPE = arm64
+
+ifeq ($(BR2_PACKAGE_BROADCOM_BSA_AP6255),y)
+	BTFIRMWARE = BCM4345C0.hcd
+endif
+
+ifeq ($(BR2_PACKAGE_BROADCOM_BSA_AP6212A1),y)
+	BTFIRMWARE = bcm43438a1.hcd
+endif
+
+define BROADCOM_BSA_BUILD_CMDS
+	for ff in $(BROADCOM_BSA_APP); do \
+		$(MAKE) -C $(@D)/$(BROADCOM_BSA_PATH)/$$ff/build CPU=$(BROADCOM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC) BSASHAREDLIB=TRUE; \
+	done
+endef
+
+define BROADCOM_BSA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/server/$(BROADCOM_BSA_BUILD_TYPE)/bsa_server \
+		$(TARGET_DIR)/usr/bin/bsa_server
+	$(INSTALL) -D -m 755 $(@D)/$(BROADCOM_BSA_PATH)/$(BROADCOM_BSA_LIBBSA)/build/$(BROADCOM_BSA_BUILD_TYPE)/sharedlib/libbsa.so \
+		$(TARGET_DIR)/usr/lib/libbsa.so
+	for ff in $(BROADCOM_BSA_APP); do \
+		$(INSTALL) -D -m 755 $(@D)/$(BROADCOM_BSA_PATH)/$${ff}/build/$(BROADCOM_BSA_BUILD_TYPE)/$${ff} $(TARGET_DIR)/usr/bin/${ff}; \
+	done
+
+	mkdir -p $(TARGET_DIR)/etc/bsa_file
+	$(INSTALL) -D -m 755 $(TOPDIR)/../external/bluetooth_bsa/test_files/av/8k8bpsMono.wav $(TARGET_DIR)/etc/bsa_file/
+	$(INSTALL) -D -m 755 $(TOPDIR)/../external/bluetooth_bsa/test_files/av/8k16bpsStereo.wav $(TARGET_DIR)/etc/bsa_file/
+	$(INSTALL) -D -m 755 package/rockchip/broadcom_bsa/bsa_bt_sink.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 755 package/rockchip/broadcom_bsa/bsa_bt_source.sh $(TARGET_DIR)/usr/bin/
+	sed -i 's/BTFIRMWARE_PATH/\/system\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_bt_sink.sh
+	sed -i 's/BTFIRMWARE_PATH/\/system\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_bt_source.sh
+
+endef
+
+$(eval $(generic-package))
---
+++ package/rockchip/broadcom_bsa/bsa_bt_sink.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+hcd_file="BTFIRMWARE_PATH"
+echo "hcd_file = $hcd_file"
+
+case "$1" in
+    start)
+
+    echo 0 > /sys/class/rfkill/rfkill0/state
+    sleep 3
+    echo 1 > /sys/class/rfkill/rfkill0/state
+    sleep 3
+
+    mkdir /data/bsa 2>/dev/null
+    mkdir /data/bsa/config 2>/dev/null
+    cd /data/bsa/config
+    echo "start broadcom bluetooth server bsa_sever"
+    killall bsa_server
+    bsa_server -r 12 -b /data/bsa/btsnoop.log -p $hcd_file -d /dev/ttyS4 > /data/bsa/bsa_log &
+    sleep 2
+
+    echo "start broadcom bluetooth app_manager"
+    app_manager -s > /data/bsa/app_mananger.log &
+
+    echo "start broadcom bluetooth app_avk"
+    app_avk -s > /data/bsa/app_avk.log &
+    sleep 1
+
+
+    echo "#########act as a bluetooth music player#########"
+    app_socket avk 2
+    cd - > /dev/null
+    sleep 2
+    echo "|----- bluetooth music player ------|"
+
+        ;;
+    stop)
+        echo -n "Stopping broadcom bsa bluetooth server & app"
+        sleep 1
+        sleep 2
+        echo 0 > /sys/class/rfkill/rfkill0/state
+        echo "|-----bluetooth music player is close-----|"
+
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+esac
+
+exit $?
+
