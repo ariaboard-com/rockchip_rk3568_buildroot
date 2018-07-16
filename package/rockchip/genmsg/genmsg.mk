41f559e6ffa73630cbae4ccd939576d085431067
zain.wang <wzz@rock-chips.com>
2018-07-16 16:56:35 +0800

    package: rockchip: genmsg: install to staging
    Change-Id: I3d2e943032194cf0db4e17d285b102ae121d82c0
    Signed-off-by: zain.wang <wzz@rock-chips.com>

--- package/rockchip/genmsg/genmsg.mk
+++ package/rockchip/genmsg/genmsg.mk
@@ -1,6 +1,7 @@
 GENMSG_VERSION = 0.5.11
 GENMSG_SITE = https://github.com/ros/genmsg/archive
 GENMSG_SOURCE = $(GENMSG_VERSION).tar.gz
+GENMSG_INSTALL_STAGING = YES
 GENMSG_DEPENDENCIES += host-catkin
 
 GENMSG_CONF_OPTS += \
