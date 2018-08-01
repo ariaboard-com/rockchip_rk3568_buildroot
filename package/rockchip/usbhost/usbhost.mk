2ed4f94c8cb37c3b4632055a6b9df90ee85601bc
Eddie Cai <eddie.cai.linux@gmail.com>
2018-08-01 14:45:07 +0800

    package: rockchip: usbhost: create udisk link when build rootfs
    squashfs can't create link on the runtime. so we create it when build rootfs
    
    Change-Id: If07a4411a1f1b5b0e49684618793233e102f2458
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/usbhost/usbhost.mk
+++ package/rockchip/usbhost/usbhost.mk
@@ -9,8 +9,7 @@ USBHOST_SITE = $(TOPDIR)/package/rockchip/usbhost
 USBHOST_SITE_METHOD = local
 
 define USBHOST_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/61-usb-disk-auto-mount.rules $(TARGET_DIR)/lib/udev/rules.d/
-
+	cd $(TARGET_DIR) && ln -s media/usb0 udisk && cd -
 endef
 
 $(eval $(generic-package))
