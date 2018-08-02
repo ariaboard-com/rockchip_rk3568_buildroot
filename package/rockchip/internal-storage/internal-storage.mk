98fb3da61bcfcb36d46ac1a7da55e2ab9bf9c2be
Eddie Cai <eddie.cai.linux@gmail.com>
2018-08-02 22:16:21 +0800

    package: rockchip: internal-storage: fix create dir error
    Change-Id: I1ce0b98455d43798ca209097cd1bc09a0a215a7c
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/internal-storage/internal-storage.mk
+++ package/rockchip/internal-storage/internal-storage.mk
@@ -11,8 +11,7 @@ INTERNAL_STORAGE_SITE_METHOD = local
 define INTERNAL_STORAGE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/61-internal-storage.rules $(TARGET_DIR)/lib/udev/rules.d/
 	$(INSTALL) -m 0755 -D $(@D)/S30mount $(TARGET_DIR)/etc/init.d/
-	mkdir -p $(TARGET_DIR)/oem $(TARGET_DIR)/userdata
-	cd $(TARGET_DIR) && ln -s userdata data && cd -
+	cd $(TARGET_DIR) && rm -rf oem userdata data && mkdir -p oem userdata && ln -s userdata data && cd -
 
 endef
 
