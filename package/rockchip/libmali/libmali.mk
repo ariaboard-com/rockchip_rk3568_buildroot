0a3c576538576fdeac6201aaa0d2351c45cf6c0f
Eddie Cai <eddie.cai.linux@gmail.com>
2018-08-02 14:50:36 +0800

    package: rockchip: libmali: fix install fail issue
    Change-Id: I2c3063ff67bf8e3062b1753b85397043765bf974
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/libmali/libmali.mk
+++ package/rockchip/libmali/libmali.mk
@@ -8,40 +8,79 @@ LIBMALI_VERSION = develop
 LIBMALI_SITE = $(TOPDIR)/../external/libmali
 LIBMALI_SITE_METHOD = local
 
-define LIBMALI_INSTALL_TARGET_CMDS
+define LIBMALI_RM_SO
+	$(TARGET_DIR)/usr/lib/libmali* \
+	$(TARGET_DIR)/usr/lib/libEGL.so* \
+	$(TARGET_DIR)/usr/lib/libgbm.so* \
+	$(TARGET_DIR)/usr/lib/libGLESv1_CM.so* \
+	$(TARGET_DIR)/usr/lib/libGLESv2.so* \
+	$(TARGET_DIR)/usr/lib/libMaliOpenCL.so \
+	$(TARGET_DIR)/usr/lib/libOpenCL.so \
+	$(TARGET_DIR)/usr/lib/libwayland-egl.so*
+endef
+
+define LIBMALI_LINK_SO
+	ln -s libmali.so libEGL.so && \
+	ln -s libmali.so libEGL.so.1 && \
+	ln -s libmali.so libgbm.so && \
+	ln -s libmali.so libgbm.so.1 && \
+	ln -s libmali.so libGLESv1_CM.so && \
+	ln -s libmali.so libGLESv1_CM.so.1 && \
+	ln -s libmali.so libGLESv2.so && \
+	ln -s libmali.so libGLESv2.so.2 && \
+	ln -s libmali.so libMaliOpenCL.so && \
+	ln -s libmali.so libOpenCL.so && \
+	ln -s libmali.so libwayland-egl.so && \
+	ln -s libmali.so libwayland-egl.so.1
+endef
+
 ifeq ($(BR2_PACKAGE_RK3308),y)
+define LIBMALI_INSTALL_TARGET_CMDS
+endef
 endif
-ifeq ($(BR2_PACKAGE_RK3326)$(BR2_PACKAGE_PX30),y)
+
+ifeq ($(BR2_PACKAGE_RK3326),y)
+define LIBMALI_INSTALL_TARGET_CMDS
+	$(LIBMALI_RM_SO)
 	$(INSTALL) -D -m 755 $(@D)/lib/libmali-bifrost-g31-rxp0-wayland-gbm.so $(TARGET_DIR)/usr/lib/
-	ln -s $(TARGET_DIR)/usr/lib/libmali-bifrost-g31-rxp0-wayland-gbm.so $(TARGET_DIR)/usr/lib/libmali.so
+	cd $(TARGET_DIR)/usr/lib/ && ln -s libmali-bifrost-g31-rxp0-wayland-gbm.so libmali.so && $(LIBMALI_LINK_SO) && cd -
+endef
 endif
+
+ifeq ($(BR2_PACKAGE_PX30),y)
+define LIBMALI_INSTALL_TARGET_CMDS
+	$(LIBMALI_RM_SO)
+        $(INSTALL) -D -m 755 $(@D)/lib/libmali-bifrost-g31-rxp0-wayland-gbm.so $(TARGET_DIR)/usr/lib/
+	cd $(TARGET_DIR)/usr/lib/ && ln -s libmali-bifrost-g31-rxp0-wayland-gbm.so libmali.so && $(LIBMALI_LINK_SO) && cd -
+endef
+endif
+
 ifeq ($(BR2_PACKAGE_PX3SE),y)
+define LIBMALI_INSTALL_TARGET_CMDS
+	rm -f $(LIBMALI_RM_SO)
 	$(INSTALL) -D -m 755 $(@D)/lib/libmali-utgard-400-r7p0-r3p0-wayland.so $(TARGET_DIR)/usr/lib/
-	ln -s $(TARGET_DIR)/usr/lib/libmali-utgard-400-r7p0-r3p0-wayland.so $(TARGET_DIR)/usr/lib/libmali.so
 	$(INSTALL) -D -m 755 $(@D)/overlay/S10libmali_px3se $(TARGET_DIR)/usr/lib/etc/init.d/S10libmali
 	$(INSTALL) -D -m 755 $(@D)/overlay/px3seBase $(TARGET_DIR)/usr/sbin/
+	cd $(TARGET_DIR)/usr/lib/ && ln -s libmali-utgard-400-r7p0-r3p0-wayland.so libmali.so && $(LIBMALI_LINK_SO) && cd -
+endef
 endif
+
 ifeq ($(BR2_PACKAGE_RK3288),y)
+define LIBMALI_INSTALL_TARGET_CMDS
+	rm -f $(LIBMALI_RM_SO)
 	$(INSTALL) -D -m 755 $(@D)/lib/aarch64-linux-gnu/libmali-midgard-t76x-r14p0-r0p0-wayland.so $(TARGET_DIR)/usr/lib/
 	$(INSTALL) -D -m 755 $(@D)/lib/aarch64-linux-gnu/libmali-midgard-t76x-r14p0-r1p0-wayland.so $(TARGET_DIR)/usr/lib/
 	$(INSTALL) -D -m 755 $(@D)/overlay/S10libmali_rk3288 $(TARGET_DIR)/usr/lib/etc/init.d/S10libmali
-endif
-ifeq ($(BR2_PACKAGE_RK3399),y)
-	$(INSTALL) -D -m 755 $(@D)/lib/aarch64-linux-gnu/libmali-midgard-t86x-r14p0-wayland.so $(TARGET_DIR)/usr/lib/
-	ln -s $(TARGET_DIR)/usr/lib/libmali-midgard-t86x-r14p0-wayland.so $(TARGET_DIR)/usr/lib/libmali.so
+	cd $(TARGET_DIR)/usr/lib/ && $(LIBMALI_LINK_SO) && cd -
+endef
 endif
 
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libEGL.so
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libEGL.so.1
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libgbm.so
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libgbm.so.1
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libGLESv1_CM.so.1
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libGLESv2.so
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libGLESv2.so.2
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libMaliOpenCL.so
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libOpenCL.so
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libwayland-egl.so
-ln -s $(TARGET_DIR)/usr/lib/libmali.so $(TARGET_DIR)/usr/lib/libwayland-egl.so.1
+ifeq ($(BR2_PACKAGE_RK3399),y)
+define LIBMALI_INSTALL_TARGET_CMDS
+	rm -f $(LIBMALI_RM_SO) 
+	$(INSTALL) -D -m 755 $(@D)/lib/aarch64-linux-gnu/libmali-midgard-t86x-r14p0-wayland.so $(TARGET_DIR)/usr/lib/
+	cd $(TARGET_DIR)/usr/lib/ && ln -s libmali-midgard-t86x-r14p0-wayland.so libmali.so && $(LIBMALI_LINK_SO) && cd -
 endef
+endif
 
 $(eval $(generic-package))
