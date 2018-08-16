f8524105e59965988619d992e64aa33f590d09be
tony.zheng <tony.zheng@rock-chips.com>
2018-08-16 17:43:24 +0800

    package: update sdcard mount rules
    Change-Id: Iab7f96a429990c5fdda33acdcd0c9fcef48afa4a
    Signed-off-by: tony.zheng <tony.zheng@rock-chips.com>

--- package/rockchip/sdmount/sdmount.mk
+++ package/rockchip/sdmount/sdmount.mk
@@ -8,6 +8,7 @@ SDMOUNT_VERSION = 0.0.1
 SDMOUNT_SITE = $(TOPDIR)/package/rockchip/sdmount
 SDMOUNT_SITE_METHOD = local
 define SDMOUNT_INSTALL_TARGET_CMDS
+        mkdir -p $(TARGET_DIR)/mnt/sdcard
 	$(INSTALL) -D -m 755 $(@D)/61-sd-cards-auto-mount.rules $(TARGET_DIR)/lib/udev/rules.d/
 endef
 
