a5c916e2c2554b41a0e66626a6867ad4ab2fc5b1
Eddie Cai <eddie.cai.linux@gmail.com>
2018-09-05 18:00:19 +0800

    package: rockchip: add libretro-mame2010
    Change-Id: I8261bc73013ce2c80b62ae7028d8c39e3cc1bf03
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

---
+++ package/rockchip/libretro-mame2010/libretro-mame2010.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# MAME2010
+#
+################################################################################
+LIBRETRO_MAME2010_VERSION = 70732f9137f6bb2bde4014746ea8bc613173dd1e
+LIBRETRO_MAME2010_SITE = $(call github,libretro,mame2010-libretro,$(LIBRETRO_MAME2010_VERSION))
+
+define LIBRETRO_MAME2010_BUILD_CMDS
+	mkdir -p $(@D)/obj/mame/cpu/ccpu
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_CC)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_BOARD)"
+endef
+
+define LIBRETRO_MAME2010_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/mame2010_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/mame0139_libretro.so
+endef
+
+$(eval $(generic-package))
