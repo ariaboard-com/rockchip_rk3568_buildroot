ac6d0a97c3baa0c11b6c2f0a8fcc37ca0ff701ad
Zorro Liu <lyx@rock-chips.com>
2018-09-10 10:57:26 +0800

    configs: delete rockchip_rk1808_mini_defconfig
    Change-Id: Ic7779f111fa70856e98dcbd6e5d9ccbb36ef872f
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -10,8 +10,7 @@ DEFCONFIG_ARRAY=("rockchip_rk3308_release" "rockchip_rk3308_32_release" "rockchi
     "rockchip_px30_32" "rockchip_px30_64" "rockchip_px30_recovery" \
     "rockchip_px3se" "rockchip_px3se_recovery" \
     "rockchip_rk3328" "rockchip_rk3328_recovery" \
-    "rockchip_rk1808" "rockchip_rk1808_recovery" \
-    "rockchip_rk1808_mini")
+    "rockchip_rk1808" "rockchip_rk1808_recovery")
 
 DEFCONFIG_ARRAY_LEN=${#DEFCONFIG_ARRAY[@]}
 
