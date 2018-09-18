4fcdb45fcfbd6cbf7b5e3e20a3bafae0e65ded9a
Lin Huang <hl@rock-chips.com>
2018-09-18 15:33:20 +0800

    rockchip_test: auto_reboot: check console-ramoops-0 message
    When reboot test, sometimes device will crash before enter system,
    this situation can not detect with now script. We can use console-ramoops-0
    to check if reboot normal last time.
    
    Change-Id: I7e5f4033e93dced0cdc7df96d01fcfb7982e087e
    Signed-off-by: Lin Huang <hl@rock-chips.com>

--- package/rockchip/rockchip_test/src/rockchip_test/auto_reboot/auto_reboot.sh
+++ package/rockchip/rockchip_test/src/rockchip_test/auto_reboot/auto_reboot.sh
@@ -30,13 +30,13 @@ else
     echo 0 > $CNT
 fi
 
-echo  Reboot after $delay seconds. 
+echo  Reboot after $delay seconds.
 
 let "cnt=$cnt+1"
 
-if [ $cnt -ge $total ] 
-then 
-    echo AutoReboot Finisned. 
+if [ $cnt -ge $total ]
+then
+    echo AutoReboot Finisned.
     echo "off" > $CNT
     echo "do cleaning ..."
     rm -rf /data/cfg/rockchip_test/auto_reboot.sh
@@ -50,11 +50,24 @@ echo "You can stop reboot by: echo off > /data/cfg/rockchip_test/reboot_cnt"
 sleep $delay
 cnt=`cat $CNT`
 if [ $cnt != "off" ]; then
-	reboot
+    if [ -e /sys/fs/pstore/ ]; then
+        echo "check console-ramoops-o message"
+        grep -q "Restarting system" /sys/fs/pstore/console-ramoops-0
+        if [ $? -ne 0 -a $cnt -ge 2 ]; then
+           echo "no found 'Restarting system' log in last time kernel message"
+           echo "consider kernel crash in last time reboot test"
+           echo "quit reboot test"
+	   exit 1
+        else
+	   reboot
+        fi
+    else
+	   reboot
+    fi
 else
-	echo "Auto reboot is off"
-	rm -rf /data/cfg/rockchip_test/auto_reboot.sh
-	rm -f $CNT
+    echo "Auto reboot is off"
+    rm -rf /data/cfg/rockchip_test/auto_reboot.sh
+    rm -f $CNT
 fi
 exit 0
 done
