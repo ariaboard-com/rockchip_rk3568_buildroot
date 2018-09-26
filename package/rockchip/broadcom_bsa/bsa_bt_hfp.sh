a61dc951671e0db49b00dcd4535e4e4945d7c8e1
ctf <ctf@rock-chips.com>
2018-09-26 17:12:23 +0800

    bluetooth: broadcom_bsa: modify bsa_bt_hfp.sh, run bsa_bt_hfp.sh start to start the HFP
    Change-Id: I007af9d95c6feb859bcab7167aa54db127f10c7d
    Signed-off-by: ctf <ctf@rock-chips.com>

--- package/rockchip/broadcom_bsa/bsa_bt_hfp.sh
+++ package/rockchip/broadcom_bsa/bsa_bt_hfp.sh
@@ -3,12 +3,45 @@
 hcd_file="BTFIRMWARE_PATH"
 echo "hcd_file = $hcd_file"
 
-mkdir -p /userdata/bsa/config/
-cd /userdata/bsa/config/
-echo 0 > /sys/class/rfkill/rfkill0/state
-sleep 1
-echo 1 > /sys/class/rfkill/rfkill0/state
-sleep 1
-bsa_server -d /dev/ttyS4 -p $hcd_file -r 13 -b /userdata/btsnoop.cfa > /userdata/bsa/bsa_log.txt &
-sleep 1
-app_manager
+case "$1" in
+    start)
+
+    echo 0 > /sys/class/rfkill/rfkill0/state
+    sleep 1
+    echo 1 > /sys/class/rfkill/rfkill0/state
+    sleep 1
+
+    mkdir -p /data/bsa/config
+    cd /data/bsa/config
+    echo "start broadcom bluetooth bsa_sever"
+    killall bsa_server
+    bsa_server -r 13 -p $hcd_file -d /dev/ttyS4 -all=0 &
+    sleep 2
+
+    echo "start broadcom bluetooth app_manager"
+    app_manager -s &
+
+    echo "start broadcom bluetooth app_hs"
+    app_hs &
+    sleep 1
+
+    echo "|----- bluetooth hfp is start------|"
+
+        ;;
+    stop)
+        echo "Stopping broadcom bsa bluetooth server & app"
+        killall app_hs
+        sleep 1
+        killall app_manager
+        sleep 1
+        killall bsa_server
+        sleep 2
+        echo "|-----bluetooth hfp is close-----|"
+
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+esac
+
+exit $?
