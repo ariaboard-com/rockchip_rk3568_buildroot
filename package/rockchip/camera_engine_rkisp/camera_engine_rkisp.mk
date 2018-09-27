621c317395ddf173f3b8d9948dbc813c7d56c961
Leo Wen <leo.wen@rock-chips.com>
2018-09-27 11:12:12 +0800

    package: rockchip: camera_engine_rkisp.mk: Add all XML files to /etc/iqfiles
    Change-Id: I1514bab72caa0eea9353854f39401df089c74b88
    Signed-off-by: Leo Wen <leo.wen@rock-chips.com>

--- package/rockchip/camera_engine_rkisp/camera_engine_rkisp.mk
+++ package/rockchip/camera_engine_rkisp/camera_engine_rkisp.mk
@@ -61,8 +61,10 @@ define CAMERA_ENGINE_RKISP_INSTALL_TARGET_CMDS
 	mkdir -p $(RKafDir)
 	mkdir -p $(RKaeDir)
 	mkdir -p $(RKawbDir)
+	mkdir -p $(TARGET_DIR)/etc/iqfiles
 	$(INSTALL) -D -m 755 $(TOPDIR)/package/rockchip/camera_engine_rkisp/set_pipeline.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 755 $(TOPDIR)/package/rockchip/camera_engine_rkisp/S50set_pipeline $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 755 $(@D)/iqfiles/*.xml $(TARGET_DIR)/etc/iqfiles/
 	$(INSTALL) -D -m 755 $(@D)/build/lib/librkisp.so $(TARGET_DIR)/usr/lib/
 	$(INSTALL) -D -m 755 $(@D)/plugins/3a/rkiq/af/$(CAMERA_ENGINE_RKISP_LIB)/librkisp_af.so $(RKafDir)/
 	$(INSTALL) -D -m 755 $(@D)/plugins/3a/rkiq/aec/$(CAMERA_ENGINE_RKISP_LIB)/librkisp_ae.so $(RKaeDir)/
