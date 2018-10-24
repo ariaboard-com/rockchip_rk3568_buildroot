50c88aa66b4b3d6a87c29aed512e00d66bd5ca92
Jeffy Chen <jeffy.chen@rock-chips.com>
2018-10-24 09:53:18 +0800

    package: libretro-lutro: Fix compile error without qemu
    Change-Id: I7c4a60e104907bc1d07452bc0157eabd8cbd264a
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/libretro-lutro/libretro-lutro.mk
+++ package/libretro-lutro/libretro-lutro.mk
@@ -6,6 +6,13 @@
 LIBRETRO_LUTRO_VERSION = b69dd3e566dfcffee17dd33049674b3d3dcd441c
 LIBRETRO_LUTRO_SITE = $(call github,libretro,libretro-lutro,$(LIBRETRO_LUTRO_VERSION))
 
+ifeq ($(BR2_arm),y)
+	LIBRETRO_LUTRO_CONF_OPTS += HOST_CC="gcc -m32"
+endif
+ifeq ($(BR2_aarch64),y)
+	LIBRETRO_LUTRO_CONF_OPTS += HOST_CC="gcc"
+endif
+
 define LIBRETRO_LUTRO_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
 	       LDFLAGS="$(TARGET_LDFLAGS)" \
@@ -13,7 +20,7 @@ define LIBRETRO_LUTRO_BUILD_CMDS
 	       CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_CC)" \
 	       RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" \
 	       TARGET_STRIP=$(TARGET_STRIP) \
-	       platform="unix"
+	       platform="unix" $(LIBRETRO_LUTRO_CONF_OPTS)
 endef
 
 define LIBRETRO_LUTRO_INSTALL_TARGET_CMDS
