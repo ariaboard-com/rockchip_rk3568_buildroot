f0c0641f2b4b0dd7496f56d6e0347203ea085b11
Zain.Wang <wzz@rock-chips.com>
2018-10-26 17:39:41 +0800

    rockchip/usbdevice: make usb config more flexible
    Add a config file for S50usbdevice
    
    Change-Id: I98ac28883d5c13b587c5ea62d64d00b969a828c2
    Signed-off-by: Zain.Wang <wzz@rock-chips.com>

--- package/rockchip/usbdevice/usbdevice.mk
+++ package/rockchip/usbdevice/usbdevice.mk
@@ -9,14 +9,39 @@ USBDEVICE_SITE = $(TOPDIR)/package/rockchip/usbdevice
 USBDEVICE_SITE_METHOD = local
 USBDEVICE_LICENSE = Apache V2.0
 USBDEVICE_LICENSE_FILES = NOTICE
+USBDEVICE_USB_CONFIG_FILE = $(TARGET_DIR)/etc/init.d/.usb_config
 
 define USBDEVICE_INSTALL_TARGET_CMDS
-	cp $(@D)/S50usbdevicebase $(@D)/S50usbdevice
-	$(@D)/geninitfile.sh $(@D)
 	$(INSTALL) -m 0755 -D $(@D)/S50usbdevice $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 0644 -D $(@D)/61-usbdevice.rules $(TARGET_DIR)/lib/udev/rules.d/
 	$(INSTALL) -m 0755 -D $(@D)/usbdevice $(TARGET_DIR)/usr/bin/
 
+	test -e $(USBDEVICE_USB_CONFIG_FILE) && rm $(USBDEVICE_USB_CONFIG_FILE)
+	touch $(USBDEVICE_USB_CONFIG_FILE)
 endef
+
+ifeq ($(BR2_PACKAGE_ANDROID_TOOLS_ADBD),y)
+define USBDEVICE_ADD_ADBD_CONFIG
+	test ! `grep usb_adb_en $(USBDEVICE_USB_CONFIG_FILE)` && \
+		echo usb_adb_en >> $(USBDEVICE_USB_CONFIG_FILE)
+endef
+USBDEVICE_POST_INSTALL_TARGET_HOOKS += USBDEVICE_ADD_ADBD_CONFIG
+endif
+
+ifeq ($(BR2_PACKAGE_MTP),y)
+define USBDEVICE_ADD_MTP_CONFIG
+	test ! `grep usb_mtp_en $(USBDEVICE_USB_CONFIG_FILE)` && \
+		echo usb_mtp_en >> $(USBDEVICE_USB_CONFIG_FILE)
+endef
+USBDEVICE_POST_INSTALL_TARGET_HOOKS += USBDEVICE_ADD_MTP_CONFIG
+endif
+
+ifeq ($(BR2_PACKAGE_USB_MASS_STORAGE),y)
+define USBDEVICE_ADD_UMS_CONFIG
+	test ! `grep usb_ums_en $(USBDEVICE_USB_CONFIG_FILE)` && \
+		echo usb_ums_en >> $(USBDEVICE_USB_CONFIG_FILE)
+endef
+USBDEVICE_POST_INSTALL_TARGET_HOOKS += USBDEVICE_ADD_UMS_CONFIG
+endif
 
 $(eval $(generic-package))
