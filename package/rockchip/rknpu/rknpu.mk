5773e58947c0a9bbf591b26771f010ab6dbf11f9
Fei Zeng <felix.zeng@rock-chips.com>
2018-11-08 10:24:09 +0800

    package: rockchip: rknpu: fixes rknn runtime and driver libraries dependency
    Change-Id: I7abb8039ec8f326da5cac78c0c7aac900566fbcf
    Signed-off-by: Fei Zeng <felix.zeng@rock-chips.com>

--- package/rockchip/rknpu/rknpu.mk
+++ package/rockchip/rknpu/rknpu.mk
@@ -21,6 +21,8 @@ define RKNPU_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/npu/
     $(INSTALL) -D -m 0644 $(@D)/drivers/$(NPU_KO_FILE) $(TARGET_DIR)/usr/lib/modules/galcore.ko
     $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(TARGET_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/rknn/usr/lib/*.so $(STAGING_DIR)/usr/lib/
     $(INSTALL) -D -m 0644 $(@D)/drivers/cl_*.h $(TARGET_DIR)/usr/lib/
     $(INSTALL) -D -m 0755 $(@D)/$(NPU_INIT_FILE) $(TARGET_DIR)/etc/init.d/S99NPU_init
     cp -r $(@D)/rknn/usr/* $(TARGET_DIR)/usr/
