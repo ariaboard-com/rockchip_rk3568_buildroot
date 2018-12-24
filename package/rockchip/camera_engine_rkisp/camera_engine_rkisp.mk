a1ebca19b1ccea35d0f12f027d47af1c7640b8b7
Leo Wen <leo.wen@rock-chips.com>
2018-12-24 15:06:01 +0800

    package: rockchip: camera_engine_rkisp: remove set pipeline
    Change-Id: Ib8d1a35ebd5717401009beb1e580cf6eaae5e7f8
    Signed-off-by: Leo Wen <leo.wen@rock-chips.com>

--- package/rockchip/camera_engine_rkisp/camera_engine_rkisp.mk
+++ package/rockchip/camera_engine_rkisp/camera_engine_rkisp.mk
@@ -62,9 +62,8 @@ define CAMERA_ENGINE_RKISP_INSTALL_TARGET_CMDS
 	mkdir -p $(RKaeDir)
 	mkdir -p $(RKawbDir)
 	mkdir -p $(TARGET_DIR)/etc/iqfiles
-	$(INSTALL) -D -m 755 $(TOPDIR)/package/rockchip/camera_engine_rkisp/set_pipeline.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 755 $(TOPDIR)/package/rockchip/camera_engine_rkisp/camera_rkisp.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 755 $(TOPDIR)/package/rockchip/camera_engine_rkisp/S50set_pipeline $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 755 $(TOPDIR)/package/rockchip/camera_engine_rkisp/S50link_iq $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -D -m 755 $(@D)/build/bin/rkisp_demo $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 644 $(@D)/iqfiles/*.xml $(TARGET_DIR)/etc/iqfiles/
 	$(INSTALL) -D -m 644 $(@D)/build/lib/librkisp.so $(TARGET_DIR)/usr/lib/
