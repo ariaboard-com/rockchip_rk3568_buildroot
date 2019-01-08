8a7a11c5b63a0fb2df10696b2215690fc10d5283
Shunqian Zheng <zhengsq@rock-chips.com>
2019-01-08 09:13:32 +0800

    package: update: fix build error without libdrm,libpng
    This is a copy-paste from recovery, deleting RECOVERY_BUILD_OPTS.
    
    Change-Id: Idca1fff1a5cc7418fd12004583db627ab173dd28
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>

--- package/rockchip/update/update.mk
+++ package/rockchip/update/update.mk
@@ -10,7 +10,7 @@ UPDATE_LICENSE = Apache V2.0
 UPDATE_BUILD_OPTS+=-I$(PROJECT_DIR)/update_recv/
 
 define UPDATE_BUILD_CMDS
-	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) $(RECOVERY_BUILD_OPTS) \
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
 		package/rockchip/update/update.c package/rockchip/update/update_recv/update_recv.c -o $(@D)/update
 endef
 
