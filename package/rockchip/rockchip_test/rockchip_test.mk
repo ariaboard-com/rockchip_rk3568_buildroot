17745038beae28e889f172e4f751d02f0f939d24
Caesar Wang <wxt@rock-chips.com>
2019-01-10 14:21:16 +0800

    package/rockchip: support arm and aarch64 for video
    Change-Id: Ib99cc519e2228bb372c5e8f4785d5d1dbd457faa
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>

--- package/rockchip/rockchip_test/rockchip_test.mk
+++ package/rockchip/rockchip_test/rockchip_test.mk
@@ -5,9 +5,17 @@ ROCKCHIP_TEST_VERSION = 20190107
 ROCKCHIP_TEST_SITE_METHOD = local
 ROCKCHIP_TEST_SITE = $(TOPDIR)/package/rockchip/rockchip_test/src
 
+ifeq ($(BR2_PACKAGE_RK1808),y)
 define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
-    cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
-    cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
+	cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
+	cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
+	cp -rf $(@D)/npu ${TARGET_DIR}/rockchip_test/
 endef
+else
+define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
+	cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
+	cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
+endef
+endif
 
 $(eval $(generic-package))
