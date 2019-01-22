ee61838fcafc59a60c7b6a3821d601ec91e55770
Ziyuan Xu <xzy.xu@rock-chips.com>
2019-01-22 17:09:32 +0800

    package: iniparser: rename target library to libiniparser.so
    Change-Id: I11a1122959af0142f28be04f53ac0676938be5e9
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- package/iniparser/iniparser.mk
+++ package/iniparser/iniparser.mk
@@ -11,16 +11,16 @@ INIPARSER_LICENSE = MIT
 INIPARSER_LICENSE_FILES = LICENSE
 
 define INIPARSER_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) SO_TARGET=libiniparser.so -C $(@D)
 endef
 
 define INIPARSER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libiniparser.so* $(TARGET_DIR)/usr/lib
+	$(INSTALL) -D -m 0755 $(@D)/libiniparser.so $(TARGET_DIR)/usr/lib
 	$(INSTALL) -D -m 0644 $(@D)/src/*.h $(TARGET_DIR)/usr/include
 endef
 
 define INIPARSER_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libiniparser.so* $(STAGING_DIR)/usr/lib/
+	$(INSTALL) -D -m 0755 $(@D)/libiniparser.so $(STAGING_DIR)/usr/lib/
 	$(INSTALL) -D -m 0644 $(@D)/src/*.h $(STAGING_DIR)/usr/include
 endef
 
