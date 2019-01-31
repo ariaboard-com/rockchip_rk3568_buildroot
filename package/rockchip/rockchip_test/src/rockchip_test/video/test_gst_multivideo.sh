c397477158bea91f5eab788361ae87841b41e26e
Caesar Wang <wxt@rock-chips.com>
2019-01-31 18:14:08 +0800

    package/rockchip: fixes test_gst_multivideo.sh for rockchip_test
    Change-Id: Ided7b3f09b95fc28b618ee941f668cdc1b7602eb
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- package/rockchip/rockchip_test/src/rockchip_test/video/test_gst_multivideo.sh
+++ package/rockchip/rockchip_test/src/rockchip_test/video/test_gst_multivideo.sh
@@ -13,22 +13,22 @@ sleep 1
 
 case "$1" in
 	test)
-		videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
+		./videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
 		sleep 1
-		videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
+		./videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
 		sleep 1
-		videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
+		./videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
 		sleep 1
-		videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
+		./videowidget file:///oem/SampleVideo_1280x720_5mb.mp4&
 		;;
 	$1)
-		videowidget file:///$1&
+		./videowidget file:///$1&
 		sleep 1
-		videowidget file:///$1&
+		./videowidget file:///$1&
 		sleep 1
-		videowidget file:///$1&
+		./videowidget file:///$1&
 		sleep 1
-		videowidget file:///$1&
+		./videowidget file:///$1&
 		;;
 esac
 shift
