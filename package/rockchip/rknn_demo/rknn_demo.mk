6b62fecce660df1984f0584f93be6d0ef91b0048
Lin Huang <hl@rock-chips.com>
2019-02-01 16:25:55 +0800

    package: rockchip: rknn_demo: support joint demo
    Change-Id: I23589bd930460777cdbbca14a458433a9b8aaab3
    Signed-off-by: Lin Huang <hl@rock-chips.com>

--- package/rockchip/rknn_demo/rknn_demo.mk
+++ package/rockchip/rknn_demo/rknn_demo.mk
@@ -9,8 +9,23 @@ RKNN_DEMO_SITE = $(TOPDIR)/../external/rknn_demo
 RKNN_DEMO_SITE_METHOD = local
 RKNN_DEMO_DEPENDENCIES = jpeg libpng12 libv4l rknpu linux-rga minigui
 
-RKNN_DEMO_CONF_OPTS += -DENABLE_SSD=1
+ifeq ($(BR2_PACKAGE_RKNN_DEMO_ENABLE_JOINT),y)
+	RKNN_DEMO_CONF_OPTS += -DENABLE_JOINT=1
+else
+	RKNN_DEMO_CONF_OPTS += -DENABLE_SSD=1
+endif
 
+
+ifeq ($(BR2_PACKAGE_RKNN_DEMO_ENABLE_JOINT),y)
+define RKNN_DEMO_INSTALL_TARGET_CMDS
+		mkdir -p $(TARGET_DIR)/usr/share/rknn_demo/
+		mkdir -p $(TARGET_DIR)/usr/local/share/
+		$(INSTALL) -D -m 0644 $(@D)/rknn/joint/cpm.rknn $(TARGET_DIR)/usr/share/rknn_demo/
+		$(INSTALL) -D -m 0644 $(@D)/minigui/MiniGUI.cfg $(TARGET_DIR)/etc
+		cp -r $(@D)/minigui/ $(TARGET_DIR)/usr/local/share/
+		$(INSTALL) -D -m 0755 $(@D)/rknn_demo $(TARGET_DIR)/usr/bin
+endef
+else
 define RKNN_DEMO_INSTALL_TARGET_CMDS
 		mkdir -p $(TARGET_DIR)/usr/share/rknn_demo/
 		mkdir -p $(TARGET_DIR)/usr/local/share/
@@ -21,5 +36,6 @@ define RKNN_DEMO_INSTALL_TARGET_CMDS
 		cp -r $(@D)/minigui/ $(TARGET_DIR)/usr/local/share/
 		$(INSTALL) -D -m 0755 $(@D)/rknn_demo $(TARGET_DIR)/usr/bin
 endef
+endif
 
 $(eval $(cmake-package))
