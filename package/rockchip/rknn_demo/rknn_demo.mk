c4b8998b606ae2164d507b6fb1f7bc3de2d0d3ab
Hertz Wang <wangh@rock-chips.com>
2019-02-25 14:23:50 +0800

    rknn_demo: make install in CMakeLists.txt instead of mk
    Change-Id: I122ffbb52dd97d51ed0c5cebc48c6059cd8ea9ed
    Signed-off-by: Hertz Wang <wangh@rock-chips.com>

--- package/rockchip/rknn_demo/rknn_demo.mk
+++ package/rockchip/rknn_demo/rknn_demo.mk
@@ -40,41 +40,38 @@ else
 	RKNN_DEMO_CONF_OPTS += $(RKNN_DEMO_USER_CONF_OPTS)
 endif
 
+ifeq ($(BR2_PACKAGE_RK1808),y)
+RKNN_DEMO_MINIGUI_CFG=minigui/MiniGUI-1280x720.cfg
+endif
+
+ifeq ($(BR2_PACKAGE_RK3399PRO),y)
+RKNN_DEMO_MINIGUI_CFG=minigui/MiniGUI-2048x1536.cfg
+endif
+
+RKNN_DEMO_CONF_OPTS += -DMINIGUI_CFG=$(RKNN_DEMO_MINIGUI_CFG)
+
 ifeq ($(BR2_PACKAGE_RKNN_DEMO_ENABLE_JOINT),y)
-define RKNN_DEMO_INSTALL_TARGET_CMDS
-		mkdir -p $(TARGET_DIR)/usr/share/rknn_demo/
-		mkdir -p $(TARGET_DIR)/usr/local/share/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/joint/cpm.rknn $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/minigui/MiniGUI.cfg $(TARGET_DIR)/etc
-		cp -r $(@D)/minigui/ $(TARGET_DIR)/usr/local/share/
-		$(INSTALL) -D -m 0755 $(@D)/rknn_demo $(TARGET_DIR)/usr/bin
-endef
+RKNN_MODEL_RESOURCE_FILES = rknn/joint/cpm.rknn
+endif
+
+ifeq ($(BR2_PACKAGE_RKNN_DEMO_ENABLE_FRG),y)
+RKNN_MODEL_RESOURCE_FILES = rknn/frg/frgsdk_rk1808/model/align.rknn \
+							rknn/frg/frgsdk_rk1808/model/detect.rknn \
+							rknn/frg/frgsdk_rk1808/model/recognize.rknn
 endif
 
 ifeq ($(BR2_PACKAGE_RKNN_DEMO_ENABLE_SSD),y)
 ifeq ($(BR2_PACKAGE_RK1808),y)
-define RKNN_DEMO_INSTALL_TARGET_CMDS
-		mkdir -p $(TARGET_DIR)/usr/share/rknn_demo/
-		mkdir -p $(TARGET_DIR)/usr/local/share/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/ssd/ssd_1808/ssd_inception_v2.rknn $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/ssd/ssd_1808/coco_labels_list.txt $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/ssd/ssd_1808/box_priors.txt $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/minigui/MiniGUI-1280x720.cfg $(TARGET_DIR)/etc/MiniGUI.cfg
-		cp -r $(@D)/minigui/ $(TARGET_DIR)/usr/local/share/
-		$(INSTALL) -D -m 0755 $(@D)/rknn_demo $(TARGET_DIR)/usr/bin
-endef
+RKNN_MODEL_RESOURCE_FILES = rknn/ssd/ssd_1808/ssd_inception_v2.rknn \
+							rknn/ssd/ssd_1808/coco_labels_list.txt \
+							rknn/ssd/ssd_1808/box_priors.txt
 else
-define RKNN_DEMO_INSTALL_TARGET_CMDS
-		mkdir -p $(TARGET_DIR)/usr/share/rknn_demo/
-		mkdir -p $(TARGET_DIR)/usr/local/share/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/ssd/ssd_3399pro/mobilenet_ssd.rknn $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/ssd/ssd_3399pro/coco_labels_list.txt $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/rknn/ssd/ssd_3399pro/box_priors.txt $(TARGET_DIR)/usr/share/rknn_demo/
-		$(INSTALL) -D -m 0644 $(@D)/minigui/MiniGUI-2048x1536.cfg $(TARGET_DIR)/etc/MiniGUI.cfg
-		cp -r $(@D)/minigui/ $(TARGET_DIR)/usr/local/share/
-		$(INSTALL) -D -m 0755 $(@D)/rknn_demo $(TARGET_DIR)/usr/bin
-endef
+RKNN_MODEL_RESOURCE_FILES = rknn/ssd/ssd_3399pro/mobilenet_ssd.rknn \
+							rknn/ssd/ssd_3399pro/coco_labels_list.txt \
+							rknn/ssd/ssd_3399pro/box_priors.txt
 endif
 endif
+
+RKNN_DEMO_CONF_OPTS += -DMODEL_RESOURCE_FILES="$(RKNN_MODEL_RESOURCE_FILES)"
 
 $(eval $(cmake-package))
