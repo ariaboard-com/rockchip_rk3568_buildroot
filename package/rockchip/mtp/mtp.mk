e317c1221a9694574efd70458f1c1673174c7cb7
zain wang <wzz@rock-chips.com>
2019-03-01 15:44:22 +0800

    MTP: add -lpthread link to CMAKE flags
    Change-Id: I6638092a6c704f456e94d447a1abace00ba1dd68
    Signed-off-by: zain wang <wzz@rock-chips.com>
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- package/rockchip/mtp/mtp.mk
+++ package/rockchip/mtp/mtp.mk
@@ -5,4 +5,9 @@ MTP_INSTALL_STAGING = YES
 
 MTP_DEPENDENCIES = boost dbus-cpp glog
 
+define MTP_ADD_PTHREAD_TO_CMAKE_CXX_FLAGS
+	$(SED) 's%-std=c++11%-std=c++11 -lpthread%' $(@D)/CMakeLists.txt
+endef
+MTP_POST_PATCH_HOOKS += MTP_ADD_PTHREAD_TO_CMAKE_CXX_FLAGS
+
 $(eval $(cmake-package))
