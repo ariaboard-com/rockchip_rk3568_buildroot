96e26d8357a202cc2f983faedb468ad41a9a9795
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-03-05 14:19:04 +0800

    envsetup.sh: Cleanup and support choose board from arg
    For example: ./build/envsetup.sh px3se
    
    Change-Id: I06988660ce056c37c1342fe9bc6d2f09f867c9d2
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 if [ -z "${BASH_SOURCE}" ];then
-	echo Not in bash, switch to it...
-	bash -c $0
+	echo Not in bash, switching to it...
+	bash -c "$0" "$@"
 fi
 
-function get_target_board_type() {
-	echo $1 | cut -d '_' -f 2
-}
-
-function choose_type()
+function choose_board()
 {
 	echo
 	echo "You're building on Linux"
@@ -18,58 +14,32 @@ function choose_type()
 
 	echo ${DEFCONFIG_ARRAY[@]} | xargs -n 1 | sed "=" | sed "N;s/\n/. /"
 
-	local DEFAULT_NUM
-	DEFAULT_NUM=1
+	local INDEX
+	while true; do
+		read -p "Which would you like? [1]: " INDEX
+		INDEX=$((${INDEX:-1} - 1))
 
-	unset TARGET_BUILD_CONFIG
-	local ANSWER
-	while [ -z "$TARGET_BUILD_CONFIG" ]
-	do
-		echo -n "Which would you like? ["$DEFAULT_NUM"] "
-		if [ -z "$1" ]; then
-			read ANSWER
-		else
-			echo $1
-			ANSWER=$1
+		if echo $INDEX | grep -vq [^0-9]; then
+			TARGET_BUILD_CONFIG="${DEFCONFIG_ARRAY[$INDEX]}"
+			[ -n "$TARGET_BUILD_CONFIG" ] && break
 		fi
 
-		if [ -z "$ANSWER" ]; then
-			ANSWER="$DEFAULT_NUM"
-		fi
-
-		if ! echo $ANSWER | grep -q [^0-9]; then
-			if [ $ANSWER -le $DEFCONFIG_ARRAY_LEN ]; then
-				index=$((${ANSWER}-1))
-				TARGET_BUILD_CONFIG="${DEFCONFIG_ARRAY[$index]}"
-			else
-				echo
-				echo "number not in range. Please try again."
-				echo
-			fi
-		else
-			echo $ANSWER
-			TARGET_BUILD_CONFIG="$ANSWER"
-		fi
-
-		if [ -n "$1" ]; then
-			break
-		fi
+		echo
+		echo "Choice not available. Please try again."
+		echo
 	done
-
-	TARGET_DIR_NAME="$TARGET_BUILD_CONFIG"
-	export TARGET_OUTPUT_DIR="$BUILDROOT_OUTPUT_DIR/$TARGET_DIR_NAME"
 }
 
 function lunch()
 {
-	mkdir -p $TARGET_OUTPUT_DIR
-	if [ -z "$TARGET_BUILD_CONFIG" ]; then
-		return;
-	fi
+	TARGET_DIR_NAME="$TARGET_BUILD_CONFIG"
+	export TARGET_OUTPUT_DIR="$BUILDROOT_OUTPUT_DIR/$TARGET_DIR_NAME"
+
+	mkdir -p $TARGET_OUTPUT_DIR || return
 
 	echo "==========================================="
 	echo
-	echo "#TARGET_BOARD=`get_target_board_type $TARGET_BUILD_CONFIG`"
+	echo "#TARGET_BOARD=`echo $TARGET_BUILD_CONFIG | cut -d '_' -f 2`"
 	echo "#OUTPUT_DIR=output/$TARGET_DIR_NAME"
 	echo "#CONFIG=${TARGET_BUILD_CONFIG}_defconfig"
 	echo
@@ -78,18 +48,20 @@ function lunch()
 	make -C ${BUILDROOT_DIR} O="$TARGET_OUTPUT_DIR" \
 		"$TARGET_BUILD_CONFIG"_defconfig
 
-	OLD_CONF=${TARGET_OUTPUT_DIR}/.config.old
-	CONF=${TARGET_OUTPUT_DIR}/.config
-	if diff ${OLD_CONF} ${CONF} 2>/dev/null|grep -qE "is not set$|=y$";then
+	CONFIG=${TARGET_OUTPUT_DIR}/.config
+	cp ${CONFIG}{,.new}
+	mv ${CONFIG}{.old,} &>/dev/null
+
+	make -C ${BUILDROOT_DIR} O="$TARGET_OUTPUT_DIR" olddefconfig &>/dev/null
+
+	if ! diff ${CONFIG}{,.new}; then
 		read -p "Found old config, override it? (y/n):" YES
-		[ "$YES" != y ] && cp ${OLD_CONF} ${CONF}
+		[ "$YES" = "y" ] && cp ${CONFIG}{.new,}
 	fi
 }
 
-if [ "${BASH_SOURCE}" == "$0" ];then
-	echo This script is executed directly...
-	bash -c "source ${BASH_SOURCE}; bash"
-else
+function main()
+{
 	SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 	SCRIPT_DIR=$(dirname ${SCRIPT_PATH})
 	BUILDROOT_DIR=$(dirname ${SCRIPT_DIR})
@@ -102,11 +74,29 @@ else
 	alias croot="cd ${TOP_DIR}"
 
 	DEFCONFIG_ARRAY=(
-	$(cd ${BUILDROOT_DIR}/configs/; ls -v rockchip_* | sed "s/_defconfig$//")
+		$(cd ${BUILDROOT_DIR}/configs/; ls -v rockchip_* | \
+			sed "s/_defconfig$//" | grep "$1")
 	)
 
 	DEFCONFIG_ARRAY_LEN=${#DEFCONFIG_ARRAY[@]}
+	if [ $DEFCONFIG_ARRAY_LEN -eq 0 ]; then
+		echo No available configs${1:+" for: $1"}
+		return
+	fi
 
-	choose_type $@
+	if [ -n "$1" ]; then
+		TARGET_BUILD_CONFIG=${DEFCONFIG_ARRAY[0]}
+	else
+		choose_board
+	fi
+	[ -n "$TARGET_BUILD_CONFIG" ] || return
+
 	lunch
+}
+
+if [ "${BASH_SOURCE}" == "$0" ];then
+	echo This script is executed directly...
+	bash -c "source \"$0\" \"$@\"; bash"
+else
+	main "$@"
 fi
