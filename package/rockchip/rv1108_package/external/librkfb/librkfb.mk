1510b71d9f5799943a6848b0c9cd4af4f44a564a
vicent.chi <vicent.chi@rock-chips.com>
2019-03-11 15:56:13 +0800

    rv1108 package: add librkfb
    Change-Id: I9678ca5e54d86d84167cfff5461b22a0880484c1
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

---
+++ package/rockchip/rv1108_package/external/librkfb/librkfb.mk
@@ -0,0 +1,13 @@
+LIBRKFB_SITE = $(TOPDIR)/../external/librkfb
+LIBRKFB_SITE_METHOD = local
+
+ifeq ($(BR2_PACKAGE_LIBRKFB_HDMI_MAX_1080),y)
+LIBRKFB_CONF_OPTS = -DHDMI_MAX_1080=ON
+endif
+
+ifeq ($(BR2_PACKAGE_LIBRKFB_HDMI_MAX_720),y)
+LIBRKFB_CONF_OPTS = -DHDMI_MAX_720=ON
+endif
+
+
+$(eval $(cmake-package))
