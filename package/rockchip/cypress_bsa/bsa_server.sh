93f61cb34e163a8e930604062cb4fea5dd9c194e
ctf <ctf@rock-chips.com>
2019-03-14 15:57:24 +0800

    cypress: rm sleep in bsa scripts
    Change-Id: I715549d2b5ffc20c75dde98786bc1c8e3d28370a
    Signed-off-by: ctf <ctf@rock-chips.com>

--- package/rockchip/cypress_bsa/bsa_server.sh
+++ package/rockchip/cypress_bsa/bsa_server.sh
@@ -6,24 +6,24 @@ echo "hcd_file = $hcd_file"
 case "$1" in
     start)
 
-    echo 0 > /sys/class/rfkill/rfkill0/state
-    sleep 3
+    killall bsa_server
+    check_not_exist.sh bsa_server
+
     echo 1 > /sys/class/rfkill/rfkill0/state
-    sleep 3
 
     mkdir -p /data/bsa/config
     cd /data/bsa/config
     echo "start broadcom bluetooth server bsa_sever"
-    killall bsa_server
-    bsa_server -r 12 -b /data/bsa/btsnoop.log -p $hcd_file -d /dev/ttyS4 > /data/bsa/bsa_log &
-    sleep 2
+    bsa_server -r 12 -p $hcd_file -d /dev/ttyS4 -all=0 &
+    #bsa_server -r 12 -b /data/bsa/btsnoop.log -p $hcd_file -d /dev/ttyS4 > /data/bsa/bsa_log &
+
     echo "|----- bluetooth bsa server is open ------|"
 
         ;;
     stop)
         echo "Stopping broadcom bsa bluetooth server"
         killall bsa_server
-        sleep 2
+        check_not_exist.sh bsa_server
         echo 0 > /sys/class/rfkill/rfkill0/state
         echo "|-----bluetooth bsa server is close-----|"
 
