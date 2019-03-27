f467b3ce352b0705b5b685a14529db5add9c9138
Caesar Wang <wxt@rock-chips.com>
2019-03-27 16:23:43 +0800

    package/rockchip: optimize glmark2 test on rockchip_test
    The glmark2 test supports the rk3399pro Socs.
    
    Change-Id: I939c6e383fb14585f856f0761f672a4e015c36d0
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- package/rockchip/rockchip_test/src/rockchip_test/gpu/test_glmark2_offscreen.sh
+++ package/rockchip/rockchip_test/src/rockchip_test/gpu/test_glmark2_offscreen.sh
@@ -1,49 +1,34 @@
 #!/bin/bash -e
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-function set_performance() {
-if [ "$1" == "rk3288" ];
-then
-	echo performance > /sys/class/devfreq/dmc/governor # set ddr
-	echo performance > /sys/class/devfreq/ffa30000.gpu/governor # set gpu
-	echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # set lit cpu
-	echo "set the cpu/ddr/gpu for performance"
+function run_glmark2() {
+if [ "$1" == "rk3288" ]; then
+	glmark2-es2-wayland --off-screen
 
 elif [[  "$1" == "rk3328"  ]]; then
-	echo performance > /sys/class/devfreq/dmc/governor # set ddr
-	echo performance > /sys/class/devfreq/ff300000.gpu/governor # set gpu
-	echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # set lit cpu
-	echo "set the cpu/ddr/gpu for performance"
+	glmark2-es2-wayland --off-screen
 
 elif [[  "$1" == "rk3399"  ]]; then
-	echo performance > /sys/class/devfreq/dmc/governor # set ddr
-	echo performance > /sys/class/devfreq/ff9a0000.gpu/governor # set gpu
-	echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # set lit cpu
-	echo performance > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor # set big cpu
-	echo "set the cpu/ddr/gpu for performance"
+	taskset -c 4-5 glmark2-es2-wayland --off-screen
+
+elif [[  "$1" == "rk3399pro"  ]]; then
+	taskset -c 4-5 glmark2-es2-wayland --off-screen
 
 elif [[  "$1" == "px30" || "$1" == "rk3326"  ]]; then
-	echo performance > /sys/class/devfreq/dmc/governor # set ddr
-	echo performance > /sys/class/devfreq/ff400000.gpu/governor # set gpu
-	echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # set lit cpu
-	echo "set the cpu/ddr/gpu for performance"
+	glmark2-es2-wayland --off-screen
 
 elif [[  "$1" == "rk1808" || "$1" == "rk3308"  ]]; then
 	echo "the chips didn't support gpu"
 
 elif [[  "$1" == "px3se"  ]]; then
-	echo performance > /sys/class/devfreq/dmc/governor # set ddr
-	echo performance > /sys/class/devfreq/10091000.gpu/governor # set gpu
-	echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # set lit cpu
-	echo "set the cpu/ddr/gpu for performance"
+	glmark2-es2-wayland --off-screen
 else
 	echo "please check if the linux support it!!!!!!!"
 fi
 }
 
 COMPATIBLE=$(cat /proc/device-tree/compatible)
-if [[ $COMPATIBLE =~ "rk3288" ]];
-then
+if [[ $COMPATIBLE =~ "rk3288" ]]; then
     CHIPNAME="rk3288"
 elif [[ $COMPATIBLE =~ "rk3308" ]]; then
     CHIPNAME="rk3308"
@@ -55,6 +40,8 @@ elif [[ $COMPATIBLE =~ "rk3326" ]]; then
     CHIPNAME="rk3326"
 elif [[ $COMPATIBLE =~ "rk3399" ]]; then
     CHIPNAME="rk3399"
+elif [[ $COMPATIBLE =~ "rk3399" && $COMPATIBLE =~ "rk3399pro"]]; then
+    CHIPNAME="rk3399pro"
 elif [[ $COMPATIBLE =~ "rk1808" ]]; then
     CHIPNAME="rk1808"
 elif [[ $COMPATIBLE =~ "px3se" ]]; then
@@ -64,9 +51,10 @@ else
 fi
 COMPATIBLE=${COMPATIBLE#rockchip,}
 
-set_performance ${CHIPNAME}
+echo performance | tee $(find /sys/ -name *governor)
 
+echo "run glmark2 wayland with offscreen......"
 export XDG_RUNTIME_DIR=/tmp/.xdg
-glmark2-es2-wayland --off-screen
+run_glmark2 ${CHIPNAME}
 
 echo "the governor is performance for now, please restart it........"
