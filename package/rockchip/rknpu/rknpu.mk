831d517a1361b97eb06860624cf9b1e0091d428b
Randall Zhuo <randall.zhuo@rock-chips.com>
2019-04-08 16:11:12 +0800

    remove cl_viv_vx_ext.h for opensource
    Change-Id: I6479011d6b2ac4f936ee522bf21f664242f1c716
    Signed-off-by: Randall Zhuo <randall.zhuo@rock-chips.com>

--- package/rockchip/rknpu/rknpu.mk
+++ package/rockchip/rknpu/rknpu.mk
@@ -27,7 +27,6 @@ define RKNPU_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(TARGET_DIR)/usr/lib/
     $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(STAGING_DIR)/usr/lib/
     $(INSTALL) -D -m 0644 $(@D)/rknn/usr/lib/*.so $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/cl_*.h $(TARGET_DIR)/usr/lib/
     $(INSTALL) -D -m 0755 $(@D)/$(NPU_INIT_FILE) $(TARGET_DIR)/etc/init.d/S99NPU_init
     cp -r $(@D)/rknn/usr/* $(TARGET_DIR)/usr/
 
