2cacbe711ae3ad74f115d307fc4775db2ad77573
lqh <kevin.lin@rock-chips.com>
2019-04-25 00:39:34 -0700

    fs-overlay-pcba modify.
    Change-Id: Ib47f8b05f534eb898df3a172d623f57c2f180809
    Signed-off-by: linqihao <kevin.lin@rock-chips.com>

--- board/rockchip/rk3308/fs-overlay-pcba/data/RkLunch.sh
+++ board/rockchip/rk3308/fs-overlay-pcba/data/RkLunch.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
+#/pcba/acodec-gain.sh &
+/data/echo_pcbatest_server &
+#/data/echo_laohua_test &
+aplay /userdata/Mute.wav &
+sleep 1
+/data/acodec-gain.sh &
+sleep 1
 
-echo_pcbatest_server &
-echo_auto_test echo_wlan_test &
-echo_auto_test echo_bt_test &
-echo_auto_test echo_ddr_test &
 
-#echo_auto_test echo_emmc_test &
-#echo_auto_test echo_rtc_test &
