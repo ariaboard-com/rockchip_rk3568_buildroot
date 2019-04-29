abab545ec46ca367a9820170b6230e78c9d5d9b3
Nickey Yang <nickey.yang@rock-chips.com>
2019-04-29 21:13:38 +0800

    package: QLauncher: fix build err.
    Change-Id: I7f26f59f67424114b18886b3d29b74b9eb626123
    Signed-off-by: Nickey Yang <nickey.yang@rock-chips.com>

--- package/rockchip/QLauncher/QLauncher.mk
+++ package/rockchip/QLauncher/QLauncher.mk
@@ -21,6 +21,7 @@ endef
 
 define QLAUNCHER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/QLauncher
+	mkdir -p $(TARGET_DIR)/usr/share/backgrounds
 	cp $(BUILD_DIR)/QLauncher-$(QLAUNCHER_VERSION)/resources/images/* $(TARGET_DIR)/usr/share/backgrounds/
 	$(INSTALL) -D -m 0755 $(@D)/QLauncher \
 		$(TARGET_DIR)/usr/local/QLauncher/QLauncher
