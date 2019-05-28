b9a9b4b7bfde8e97d825b73be4424a27922be752
jkand.huang <jkand.huang@rock-chips.com>
2019-05-28 16:53:06 +0800

    package/rockchip/recovery:     support for updateEngine.
    Change-Id: Idf3186634763e76c1ca6b5d362b1944f68324751
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>

--- package/rockchip/recovery/recovery.mk
+++ package/rockchip/recovery/recovery.mk
@@ -15,13 +15,32 @@ PROJECT_DIR="$(@D)"
 RECOVERY_BUILD_OPTS+=-I$(PROJECT_DIR) -I$(STAGING_DIR)/usr/include/libdrm \
 	--sysroot=$(STAGING_DIR) \
 	-fPIC \
-	-lpthread
+	-lpthread \
+	-lcurl \
+	-lssl \
+	-lcrypto
 
-ifeq ($(BR2_PACKAGE_RK3308)$(BR2_PACKAGE_RK3326)$(BR2_PACKAGE_PX30),y)
+ifeq ($(BR2_PACKAGE_RK3308),y)
 	TARGET_MAKE_ENV += RecoveryNoUi=true
 else
 	RECOVERY_BUILD_OPTS += -lz -lpng -ldrm
 	RECOVERY_DEPENDENCIES += libzlib libpng libdrm
+endif
+
+ifeq ($(BR2_PACKAGE_RECOVERY_USE_RKUPDATE),y)
+	TARGET_CFLAGS += -DUSE_RKUPDATE=ON
+endif
+
+ifeq ($(BR2_PACKAGE_RECOVERY_USE_UPDATEENGINE),y)
+	TARGET_CFLAGS += -DUSE_UPDATEENGINE=ON
+endif
+
+ifeq ($(BR2_PACKAGE_RECOVERY_SUCCESSFUL_BOOT),y)
+	TARGET_CFLAGS += -DSUCCESSFUL_BOOT=ON
+endif
+
+ifeq ($(BR2_PACKAGE_RECOVERY_RETRY),y)
+	TARGET_CFLAGS += -DRETRY_BOOT=ON
 endif
 
 RECOVERY_MAKE_OPTS = \
@@ -37,11 +56,31 @@ define RECOVERY_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CC="$(TARGET_CC)" $(RECOVERY_MAKE_OPTS)
 endef
 
+ifeq ($(BR2_PACKAGE_RECOVERY_RECOVERYBIN),y)
+define RECOVERYBIN_INSTALL_TARGET
+	$(INSTALL) -D -m 755 $(@D)/recovery $(TARGET_DIR)/usr/bin/ && $(RECOVERY_IMAGE_COPY)
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_RECOVERY_BOOTCONTROL), y)
+define BOOTCONTROLBIN_INSTALL_TARGET
+	$(INSTALL) -D -m 755 $(@D)/update_engine/S99_bootcontrol $(TARGET_DIR)/etc/init.d/
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_RECOVERY_UPDATEENGINEBIN),y)
+define UPDATEENGINEBIN_INSTALL_TARGET
+	$(INSTALL) -D -m 755 $(@D)/updateEngine $(TARGET_DIR)/usr/bin/
+endef
+endif
+
 define RECOVERY_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 755 $(@D)/recovery $(TARGET_DIR)/usr/bin/ && $(RECOVERY_IMAGE_COPY)
+	$(RECOVERYBIN_INSTALL_TARGET)
+	$(UPDATEENGINEBIN_INSTALL_TARGET)
+	$(BOOTCONTROLBIN_INSTALL_TARGET)
 endef
 
-ifeq ($(BR2_PACKAGE_RECOVERY),y)
+ifeq ($(BR2_PACKAGE_RECOVERY_RECOVERYBIN),y)
 define RECOVERY_IMAGE_PACK
 	mkdir -p $(TARGET_DIR)/res/images
 	cp $(BUILD_DIR)/recovery-$(RECOVERY_VERSION)/res/images/* $(TARGET_DIR)/res/images/
