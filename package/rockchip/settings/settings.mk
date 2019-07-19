0fba67995b0c836612cda9a4ff35db2fda873306
Eddie Cai <eddie.cai.linux@gmail.com>
2019-07-19 10:48:37 +0800

    package: rockchip: setting: move target file to /usr/bin and /usr/share/icon
    make sure you update app/setting to below commit
    
    commit 62c3d49079e049996835cc3352fc390eb787f234
    Author: Eddie Cai <eddie.cai.linux@gmail.com>
    Date:   Fri Jul 19 10:35:04 2019 +0800
    
        move target file to /usr/bin and /usr/share/icon
    
        Change-Id: I4bf607b49442a3b807d0dd4a9fac02f3415ecd30
    
    Change-Id: I6ef78491e8d7118e92ed3e5a774c011a98fc6a84
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/settings/settings.mk
+++ package/rockchip/settings/settings.mk
@@ -22,10 +22,10 @@ endef
 define SETTINGS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/settings
 	cp $(BUILD_DIR)/settings-$(SETTINGS_VERSION)/conf/* $(TARGET_DIR)/usr/local/settings/
+	cp $(BUILD_DIR)/settings-$(SETTINGS_VERSION)/conf/icon_setting.png $(TARGET_DIR)/usr/share/icon/
 	mkdir -p $(TARGET_DIR)/usr/share/applications
 	cp $(BUILD_DIR)/settings-$(SETTINGS_VERSION)/setting.desktop $(TARGET_DIR)/usr/share/applications/
-	$(INSTALL) -D -m 0755 $(@D)/settings \
-		$(TARGET_DIR)/usr/local/settings/settings
+	$(INSTALL) -D -m 0755 $(@D)/settings $(TARGET_DIR)/usr/bin/settings
 endef
 
 $(eval $(generic-package))
