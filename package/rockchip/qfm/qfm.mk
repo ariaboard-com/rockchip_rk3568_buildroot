1379e0d1a83b6743094018bc3d4f6e926a7b79e2
Eddie Cai <eddie.cai.linux@gmail.com>
2019-07-19 12:45:55 +0800

    package: rockchip: qfm: install desktop file
    Change-Id: I9acd4b9f5e9ea90051824f0ce83f18bf9e7ba57e
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/qfm/qfm.mk
+++ package/rockchip/qfm/qfm.mk
@@ -23,6 +23,7 @@ define QFM_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/applications $(TARGET_DIR)/usr/share/icon
 	$(INSTALL) -D -m 0644 $(@D)/image/icon_folder.png $(TARGET_DIR)/usr/share/icon/
 	$(INSTALL) -D -m 0755 $(@D)/qfm	$(TARGET_DIR)/usr/bin/qfm
+	$(INSTALL) -D -m 0755 $(@D)/qfm.desktop	$(TARGET_DIR)/usr/share/applications/
 endef
 
 $(eval $(generic-package))
