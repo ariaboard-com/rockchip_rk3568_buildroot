1991b59fd757af3ac565be70eee45a91344e2335
Eddie Cai <eddie.cai.linux@gmail.com>
2019-07-19 10:50:29 +0800

    package: rockchip: music: move target file to /usr/bin and /usr/share/icon
    make sure you update app/music to below commit
    
    commit 16c55988e793ff693b00c096544db0b441026e86
    Author: Eddie Cai <eddie.cai.linux@gmail.com>
    Date:   Fri Jul 19 10:34:12 2019 +0800
    
        move target file to /usr/bin and /usr/share/icon
    
        Change-Id: I0db721c53c6110a30bb85baf0ed89bc22e54180a
    
    Change-Id: I68ae4ee68bfa5931c1c9656ca4236075abf51a7a
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/music/music.mk
+++ package/rockchip/music/music.mk
@@ -22,10 +22,10 @@ endef
 define MUSIC_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/music
 	cp $(BUILD_DIR)/music-$(MUSIC_VERSION)/conf/* $(TARGET_DIR)/usr/local/music/
+	cp $(BUILD_DIR)/music-$(MUSIC_VERSION)/conf/icon_music.png $(TARGET_DIR)/usr/share/icon/
 	mkdir -p $(TARGET_DIR)/usr/share/applications
 	cp $(BUILD_DIR)/music-$(MUSIC_VERSION)/music.desktop $(TARGET_DIR)/usr/share/applications/
-	$(INSTALL) -D -m 0755 $(@D)/musicPlayer \
-		$(TARGET_DIR)/usr/local/music/musicPlayer
+	$(INSTALL) -D -m 0755 $(@D)/musicPlayer $(TARGET_DIR)/usr/bin/musicPlayer
 endef
 
 $(eval $(generic-package))
