b361af6225961b2d8ac47d9fef7ab8e6137c4d06
Eddie Cai <eddie.cai.linux@gmail.com>
2019-07-19 08:31:13 +0800

    package: rockchip: qfm: fix build error
    Change-Id: I2ecda273cf748ddea14d8f13a1f76e66619de3e6
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/qfm/qfm.mk
+++ package/rockchip/qfm/qfm.mk
@@ -4,22 +4,22 @@
 #
 ################################################################################
 
-QLAUNCHER_VERSION = 1.0
-QLAUNCHER_SITE = $(TOPDIR)/../app/qfm
-QLAUNCHER_SITE_METHOD = local
+QFM_VERSION = 1.0
+QFM_SITE = $(TOPDIR)/../app/qfm
+QFM_SITE_METHOD = local
 
-QLAUNCHER_LICENSE = Apache V2.0
-QLAUNCHER_LICENSE_FILES = NOTICE
+QFM_LICENSE = Apache V2.0
+QFM_LICENSE_FILES = NOTICE
 
-define QLAUNCHER_CONFIGURE_CMDS
+define QFM_CONFIGURE_CMDS
 	cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/qmake
 endef
 
-define QLAUNCHER_BUILD_CMDS
+define QFM_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
-define QLAUNCHER_INSTALL_TARGET_CMDS
+define QFM_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/qfm	$(TARGET_DIR)/usr/bin/qfm
 endef
 
