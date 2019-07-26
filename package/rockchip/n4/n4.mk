2fb0c005e153d499d2d034c41fe115eb4bd643d7
Hertz Wang <wangh@rock-chips.com>
2019-07-26 08:05:03 +0000

    n4: cp rk-npu-usb-msg.h to staging dir
    Change-Id: Icfb5347505f011ed633e65138a642c74ffbd4aaf
    Signed-off-by: Hertz Wang <wangh@rock-chips.com>

--- package/rockchip/n4/n4.mk
+++ package/rockchip/n4/n4.mk
@@ -4,6 +4,12 @@ N4_INSTALL_STAGING = NO
 
 N4_DEPENDENCIES = host-ninja libserialport libv4l rkmedia
 
+define INSTALL_N4_KERNEL_HEADER
+cp $(TOPDIR)/../kernel/include/uapi/linux/rk-npu-usb-msg.h $(STAGING_DIR)/usr/include/linux/
+endef
+
+N4_PRE_CONFIGURE_HOOKS += INSTALL_N4_KERNEL_HEADER
+
 #N4_CONF_OPTS = -DWARNINGS_AS_ERRORS=ON
 
 N4_CONF_OPTS += -G Ninja
