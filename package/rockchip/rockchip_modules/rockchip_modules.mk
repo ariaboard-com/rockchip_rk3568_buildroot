1d5539c060f5bc2b31a0f7ffdbd7c98fb240b66a
Zain Wang <wzz@rock-chips.com>
2019-07-29 17:50:34 +0800

    rockchip_module: make script config more easier
    Only pick modules configed to $(TARGET_DIR).
    
    Change-Id: I14a30e6b2a2d8543fed27bdaeadaf313454dd4da
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

--- package/rockchip/rockchip_modules/rockchip_modules.mk
+++ package/rockchip/rockchip_modules/rockchip_modules.mk
@@ -8,39 +8,49 @@ ROCKCHIP_MODULES_VERSION = 1.0.0
 ROCKCHIP_MODULES_SITE_METHOD = local
 ROCKCHIP_MODULES_SITE = $(TOPDIR)/package/rockchip/rockchip_modules/src
 
-ROCKCHIP_MODULES_EXT4_PATH = $(TOPDIR)/../kernel/fs/ext4/ext4.ko
-ROCKCHIP_MODULES_JBD2_PATH = $(TOPDIR)/../kernel/fs/jbd2/jbd2.ko
-ROCKCHIP_MODULES_MBCACHE2_PATH = $(TOPDIR)/../kernel/fs/mbcache2.ko
-ROCKCHIP_MODULES_FAT_PATH = $(TOPDIR)/../kernel/fs/fat/fat.ko
-ROCKCHIP_MODULES_VFAT_PATH = $(TOPDIR)/../kernel/fs/fat/vfat.ko
-ROCKCHIP_MODULES_NTFS_PATH = $(TOPDIR)/../kernel/fs/ntfs/ntfs.ko
+ROCKCHIP_MODULES_EXT4 = ext4.ko
+ROCKCHIP_MODULES_JBD2 = jbd2.ko
+ROCKCHIP_MODULES_MBCACHE2 = mbcache2.ko
+ROCKCHIP_MODULES_FAT = fat.ko
+ROCKCHIP_MODULES_VFAT = vfat.ko
+ROCKCHIP_MODULES_NTFS = ntfs.ko
 
-ifeq ($(BR2_PACKAGE_ROCKCHIP_MODULES_EXT4),y)
+ROCKCHIP_MODULES_PATH = /system/lib/modules
+
 define ROCKCHIP_MODULES_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/modules
-	@$(INSTALL) -D -m 0644 $(ROCKCHIP_MODULES_JBD2_PATH) $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and make modules first"
-	@$(INSTALL) -D -m 0644 $(ROCKCHIP_MODULES_MBCACHE2_PATH) $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and make modules first"
-	@$(INSTALL) -D -m 0644 $(ROCKCHIP_MODULES_EXT4_PATH) $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and make modules first"
+	mkdir -p $(TARGET_DIR)$(ROCKCHIP_MODULES_PATH)
 	$(INSTALL) -D -m 0755 $(@D)/S100load_modules $(TARGET_DIR)/etc/init.d
 endef
+
+copy_file_from_kernel = find $(TOPDIR)/../kernel/* -name "$(1)" | \
+	xargs -n1 -i cp {} $(TARGET_DIR)$(ROCKCHIP_MODULES_PATH)
+fix_script = $(SED) "/load modules/a\\	\	insmod $(ROCKCHIP_MODULES_PATH)\/$(1)" \
+	$(TARGET_DIR)/etc/init.d/S100load_modules
+add_new_module = $(call copy_file_from_kernel,$(1)) &&\
+	$(call fix_script,$(1))
+
+ifeq ($(BR2_PACKAGE_ROCKCHIP_MODULES_EXT4),y)
+define ROCKCHIP_MODULES_INSTALL_EXT4
+	$(call add_new_module,$(ROCKCHIP_MODULES_EXT4))
+	$(call add_new_module,$(ROCKCHIP_MODULES_JBD2))
+	$(call add_new_module,$(ROCKCHIP_MODULES_MBCACHE2))
+endef
+ROCKCHIP_MODULES_POST_INSTALL_TARGET_HOOKS += ROCKCHIP_MODULES_INSTALL_EXT4
 endif
 
 ifeq ($(BR2_PACKAGE_ROCKCHIP_MODULES_FAT),y)
-define ROCKCHIP_MODULES_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/modules
-	@$(INSTALL) -D -m 0644 $(ROCKCHIP_MODULES_FAT_PATH) $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and make modules first"
-	@$(INSTALL) -D -m 0644 $(ROCKCHIP_MODULES_VFAT_PATH) $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and make modules first"
-	$(INSTALL) -D -m 0755 $(@D)/S100load_modules $(TARGET_DIR)/etc/init.d
+define ROCKCHIP_MODULES_INSTALL_FAT
+	$(call add_new_module,$(ROCKCHIP_MODULES_FAT))
+	$(call add_new_module,$(ROCKCHIP_MODULES_VFAT))
 endef
+ROCKCHIP_MODULES_POST_INSTALL_TARGET_HOOKS += ROCKCHIP_MODULES_INSTALL_FAT
 endif
 
-
 ifeq ($(BR2_PACKAGE_ROCKCHIP_MODULES_NTFS),y)
-define ROCKCHIP_MODULES_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/modules
-	@$(INSTALL) -D -m 0644 $(ROCKCHIP_MODULES_NTFS_PATH) $(TARGET_DIR)/usr/lib/modules || echo "Err, Please cd kernel and make modules first"
-	$(INSTALL) -D -m 0755 $(@D)/S100load_modules $(TARGET_DIR)/etc/init.d
+define ROCKCHIP_MODULES_INSTALL_NTFS
+	$(call add_new_module,$(ROCKCHIP_MODULES_NTFS))
 endef
+ROCKCHIP_MODULES_POST_INSTALL_TARGET_HOOKS += ROCKCHIP_MODULES_INSTALL_NTFS
 endif
 
 $(eval $(generic-package))
