b1c6130076b81fe8c59cf6d6085ea2c443d4a9d4
vicent.chi <vicent.chi@rock-chips.com>
2019-07-31 10:35:40 +0000

    rv1108 package: add model_loader
    Change-Id: I034ffdd55f363a6145befb94cb82cf1a6ca6ce9f
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

---
+++ package/rockchip/rv1108_package/frameworks/model_loader/model_loader.mk
@@ -0,0 +1,8 @@
+MODEL_LOADER_SITE = $(TOPDIR)/../frameworks/model_loader
+MODEL_LOADER_SITE_METHOD = local
+MODEL_LOADER_INSTALL_STAGING = YES
+
+# add dependencies
+MODEL_LOADER_DEPENDENCIES = libion adk
+
+$(eval $(cmake-package))
