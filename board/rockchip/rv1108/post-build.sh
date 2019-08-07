5b46285b15d5db067c870c6c9371431f7e46635c
vicent.chi <vicent.chi@rock-chips.com>
2019-08-07 09:06:38 +0000

    rv1108: add BR2_ROOTFS_POST_BUILD_SCRIPT
    Change-Id: I1cd98a1b064adde30a1896615f53fa7c99e18388
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

---
+++ board/rockchip/rv1108/post-build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CMD=`realpath $0`
+BUILDROOT_DIR=`pwd`
+SDK_DIR=`dirname $BUILDROOT_DIR`
+RV1108_DEVICES_BOARD_DIR=$SDK_DIR/device/rockchip/$RK_TARGET_PRODUCT/overlay-board
+
+echo "======================================================="
+echo "SDK_DIR                  = $SDK_DIR"
+echo "BUILDROOT_DIR            = $BUILDROOT_DIR"
+echo "RV1108_DEVICES_BOARD_DIR = $RV1108_DEVICES_BOARD_DIR"
+echo "RK_TARGET_PRODUCT        = $RK_TARGET_PRODUCT"
+echo "RK_TARGET_BOARD_VERSION  = $RK_TARGET_BOARD_VERSION"
+echo "======================================================="
+
+if [ ! -d  $RV1108_DEVICES_BOARD_DIR ]; then
+        echo "$RV1108_DEVICES_BOARD_DIR no exit"
+	return
+else
+	cd $RV1108_DEVICES_BOARD_DIR
+fi
+
+if [ -d  rv1108-$RK_TARGET_BOARD_VERSION ]; then
+	echo "copy rv1108-$RK_TARGET_BOARD_VERSION"
+	cp -fr rv1108-$RK_TARGET_BOARD_VERSION/* $TARGET_DIR/
+else
+	echo "rv1108-$RK_TARGET_BOARD_VERSION no exit"
+fi
+
+cd $BUILDROOT_DIR
