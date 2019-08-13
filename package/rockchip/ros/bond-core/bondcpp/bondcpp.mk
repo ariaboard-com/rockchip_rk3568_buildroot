074f11c740a9a8f12b94d45dc1df2fbfe3220b03
Zain Wang <wzz@rock-chips.com>
2019-08-13 16:50:55 +0800

    ros: bond-core: add indigo support
    Change-Id: I9db95c4bc447392ad083bc728f788cc1fde871d8
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

--- package/rockchip/ros/bond-core/bondcpp/bondcpp.mk
+++ package/rockchip/ros/bond-core/bondcpp/bondcpp.mk
@@ -1,8 +1,5 @@
-BONDCPP_VERSION = 1.8.3
-BONDCPP_SITE = https://github.com/ros/bond_core/archive
-BONDCPP_SOURCE = $(BONDCPP_VERSION).tar.gz
 BONDCPP_SUBDIR = bondcpp
 
 BONDCPP_DEPENDENCIES = bond cmake_modules roscpp smclib util-linux
 
-$(eval $(catkin-package))
+$(eval $(catkin-packages-set))
--- package/rockchip/ros/bond-core/smclib/smclib.mk
+++ package/rockchip/ros/bond-core/smclib/smclib.mk
@@ -1,6 +1,3 @@
-SMCLIB_VERSION = 1.8.3
-SMCLIB_SITE = https://github.com/ros/bond_core/archive
-SMCLIB_SOURCE = $(SMCLIB_VERSION).tar.gz
 SMCLIB_SUBDIR = smclib
 
-$(eval $(catkin-package))
+$(eval $(catkin-packages-set))
