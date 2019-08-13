ff309d5038c5fe8ece4c65c582727ebb5dc482d3
Zain Wang <wzz@rock-chips.com>
2019-08-13 17:37:32 +0800

    ros: dynamic-reconfigure: add boost dependencies
    Change-Id: Ia9267f87149a332279cfdc5c3ab0ad66bae52945
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

--- package/rockchip/ros/dynamic-reconfigure/dynamic-reconfigure.mk
+++ package/rockchip/ros/dynamic-reconfigure/dynamic-reconfigure.mk
@@ -3,8 +3,8 @@ DYNAMIC_RECONFIGURE_SITE = https://github.com/ros/dynamic_reconfigure/archive
 DYNAMIC_RECONFIGURE_SOURCE = $(DYNAMIC_RECONFIGURE_VERSION).tar.gz
 
 DYNAMIC_RECONFIGURE_CONF_OPTS += -DCATKIN_ENABLE_TESTING=False
-DYNAMIC_RECONFIGURE_DEPENDENCIES = roslib std-msgs roscpp rostest \
-								   python-rospkg
+DYNAMIC_RECONFIGURE_DEPENDENCIES = boost roslib std-msgs roscpp rostest \
+				   python-rospkg
 
 $(eval $(catkin-package))
 
