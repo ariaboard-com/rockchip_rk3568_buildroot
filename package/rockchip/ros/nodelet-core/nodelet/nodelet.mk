57a70dbc9482a1fb4b51156853c1017cfb8105e5
Zain Wang <wzz@rock-chips.com>
2019-08-13 17:03:05 +0800

    ros: nodelet-core: add indigo support
    Change-Id: I5f2aabececca3cbd348ea8e5b6eae867c7f2bf9f
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

--- package/rockchip/ros/nodelet-core/nodelet/nodelet.mk
+++ package/rockchip/ros/nodelet-core/nodelet/nodelet.mk
@@ -1,14 +1,13 @@
-NODELET_VERSION = 1.9.14
-NODELET_SITE = https://github.com/ros/nodelet_core/archive
-NODELET_SOURCE = $(NODELET_VERSION).tar.gz
 NODELET_SUBDIR = nodelet
 NODELET_DEPENDENCIES = bondcpp cmake_modules message-generation pluginlib rosconsole \
 		       roscpp std-msgs
 
+ifeq ($(BR2_PACKAGE_ROS_KINETIC),y)
 define NODELET_FIX_TINYXML2_DEPENDENCIES
 	$(SED) 's#target_link_libraries(nodeletlib $${catkin_LIBRARIES} $${BOOST_LIBRARIES})#target_link_libraries(nodeletlib $${catkin_LIBRARIES} $${BOOST_LIBRARIES} -ltinyxml2)#' \
 		$(@D)/nodelet/CMakeLists.txt
 endef
 NODELET_POST_PATCH_HOOKS += NODELET_FIX_TINYXML2_DEPENDENCIES
+endif
 
-$(eval $(catkin-package))
+$(eval $(catkin-packages-set))
