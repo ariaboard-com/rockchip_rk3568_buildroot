c1a500c548b54f1906791e2b020aac9933fb161e
frank.liu <frank.liu@rock-chips.com>
2019-08-14 14:17:57 +0800

    board: fs_overlay: support mmc and mtd mount.
    Change-Id: Ifb6f3c5d78f92603ccd297bc616b32ad0bd72960
    Signed-off-by: frank.liu <frank.liu@rock-chips.com>

--- board/rockchip/rv1108/fs-overlay/etc/profile.d/RkEnv.sh
+++ board/rockchip/rv1108/fs-overlay/etc/profile.d/RkEnv.sh
@@ -3,6 +3,6 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/lib/:/usr/lib/:/usr/local/lib/
 
 mkdir /data
-mount /dev/mmcblk0p5  /data/
-
-
+mount -t jffs2 /dev/mtdblock5 /data
+[ $? = 0 ] && echo "/data mount /dev/mtdblock5 OK" || /bin/busybox mount -t ext4 /dev/mmcblk0p5 /data
+[ $? = 0 ] && echo "/data monut /dev/mmcblk0p5 OK"
