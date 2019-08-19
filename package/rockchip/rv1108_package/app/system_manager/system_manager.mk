9208d428c2ef864e3aaff7723fb108ec10fad123
vicent.chi <vicent.chi@rock-chips.com>
2019-08-19 11:50:33 +0000

    rv1108: add S10_system_manager for init
    Change-Id: I42d2fa10af9ac2b56e2de8a4a1f4e5285a521b36
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

--- package/rockchip/rv1108_package/app/system_manager/system_manager.mk
+++ package/rockchip/rv1108_package/app/system_manager/system_manager.mk
@@ -6,6 +6,8 @@ SYSTEM_MANAGER_INSTALL_STAGING = YES
 SYSTEM_MANAGER_DEPENDENCIES = adk messenger
 
 define SYSTEM_MANAGER_INSTALL_INIT_SYSV
+    $(INSTALL) -m 0755 -D package/rockchip/rv1108_package/app/system_manager/S10_system_manager \
+		    $(TARGET_DIR)/etc/init.d/
     $(INSTALL) -m 0644 -D package/rockchip/rv1108_package/app/system_manager/system_manager.conf \
                     $(TARGET_DIR)/etc/system_manager.conf
 endef
