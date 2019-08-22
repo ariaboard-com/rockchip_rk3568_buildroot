2d4421a40f43f84fdd66193dfd41b9a20a8542ef
vicent.chi <vicent.chi@rock-chips.com>
2019-08-22 12:29:29 +0000

    board: rv1108: add data root parttion auto mount script
    Change-Id: Ie847d1747e758d96a45ccb191fdca1cb870564b0
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

--- board/rockchip/rv1108/fs-overlay/etc/profile.d/RkEnv.sh
+++ board/rockchip/rv1108/fs-overlay/etc/profile.d/RkEnv.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin
-export LD_LIBRARY_PATH=/lib/:/usr/lib/:/usr/local/lib/
-
-mkdir /data
-mount -t jffs2 /dev/mtdblock5 /data
-[ $? = 0 ] && echo "/data mount /dev/mtdblock5 OK" || /bin/busybox mount -t ext4 /dev/mmcblk0p5 /data
-[ $? = 0 ] && echo "/data monut /dev/mmcblk0p5 OK"
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/root/bin:/root/sbin
+export LD_LIBRARY_PATH=/lib/:/usr/lib/:/usr/local/lib/:/root/lib
