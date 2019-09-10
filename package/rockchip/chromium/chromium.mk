df2c81f11d2097f1d68363f354a61a891f987914
Hertz Wang <wangh@rock-chips.com>
2019-09-10 02:29:36 +0000

    chromium: update args for chromium
    Change-Id: I1e1f12d8dedb7f3a8fa84861ab990fb56d7d2b07
    Signed-off-by: Hertz Wang <wangh@rock-chips.com>

--- package/rockchip/chromium/chromium.mk
+++ package/rockchip/chromium/chromium.mk
@@ -7,16 +7,19 @@ CHROMIUM_VERSION = 74.0.3729.157
 CHROMIUM_SITE_METHOD = local
 CHROMIUM_SITE = $(TOPDIR)/../external/chromium
 
+CHROMIUM_WRAPPER_EXTRA_ARGS=CHROME_EXTRA_ARGS+=" --no-sandbox --gpu-sandbox-start-early --ignore-gpu-blacklist --enable-wayland-ime"
+
 ifeq ($(call qstrip,$(BR2_ARCH)),arm)
-define CHROMIUM_INSTALL_TARGET_CMDS
-    tar zxvf $(@D)/chromium-ozone-wayland_74.0.3729.157/$(BR2_ARCH)/chromium-ozone-wayland_armhf.tgz -C $(TARGET_DIR)/
-endef
+    CHROMIUM_ARCH := armhf
 endif
 
 ifeq ($(call qstrip,$(BR2_ARCH)),aarch64)
-define CHROMIUM_INSTALL_TARGET_CMDS
-    tar zxvf $(@D)/chromium-ozone-wayland_74.0.3729.157/$(BR2_ARCH)/chromium-ozone-wayland_aarch64.tgz -C $(TARGET_DIR)/
-endef
+    CHROMIUM_ARCH := aarch64
 endif
+
+define CHROMIUM_INSTALL_TARGET_CMDS
+    tar zxvf $(@D)/chromium-ozone-wayland_74.0.3729.157/$(BR2_ARCH)/chromium-ozone-wayland_$(CHROMIUM_ARCH).tgz -C $(TARGET_DIR)/
+    sed -i '/^CHROME_EXTRA_ARGS=/a\$(CHROMIUM_WRAPPER_EXTRA_ARGS)' $(TARGET_DIR)/usr/lib/chromium/chromium-wrapper
+endef
 
 $(eval $(generic-package))
