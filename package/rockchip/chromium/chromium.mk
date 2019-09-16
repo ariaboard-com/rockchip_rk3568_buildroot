4fcf035886fc8d646b1fd362c9f27df07485eee7
Caesar Wang <wxt@rock-chips.com>
2019-09-16 02:42:50 +0000

    package/rockchip: add the missing configure for chromium
    Change-Id: I8b76a291e905ca1bf683093768d5d7969a4c9b93
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- package/rockchip/chromium/chromium.mk
+++ package/rockchip/chromium/chromium.mk
@@ -18,6 +18,7 @@ ifeq ($(call qstrip,$(BR2_ARCH)),aarch64)
 endif
 
 define CHROMIUM_INSTALL_TARGET_CMDS
+    touch $(TARGET_DIR)/dev/video-dec0
     tar zxvf $(@D)/chromium-ozone-wayland_74.0.3729.157/$(BR2_ARCH)/chromium-ozone-wayland_$(CHROMIUM_ARCH).tgz -C $(TARGET_DIR)/
     sed -i '/^CHROME_EXTRA_ARGS=/a\$(CHROMIUM_WRAPPER_EXTRA_ARGS)' $(TARGET_DIR)/usr/lib/chromium/chromium-wrapper
 endef
