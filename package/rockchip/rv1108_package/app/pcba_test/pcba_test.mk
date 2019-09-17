05301cc74b4dc1b14ede38892f71dfadfcad449a
vicent.chi <vicent.chi@rock-chips.com>
2019-09-17 10:26:55 +0000

    package: rv1108: add pcba_test init.d scripts
    Change-Id: Ia286e9de787daf7ff4dcf2cbf04a238cc2ef3b5b
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

--- package/rockchip/rv1108_package/app/pcba_test/pcba_test.mk
+++ package/rockchip/rv1108_package/app/pcba_test/pcba_test.mk
@@ -3,10 +3,14 @@ PCBA_TEST_SITE_METHOD = local
 PCBA_TEST_INSTALL_STAGING = YES
 
 # add dependencies
-PCBA_TEST_DEPENDENCIES = rkrga rkfb adk
+PCBA_TEST_DEPENDENCIES =
 
 ifeq ($(BR2_PACKAGE_PCBA_TEST_PCTOOL_APP),y)
     PCBA_TEST_CONF_OPTS += -DUSE_PCBA_PCTOOL_APP=1
+define PCBA_TEST_INSTALL_INIT_SYSV
+    $(INSTALL) -m 0755 -D package/rockchip/rv1108_package/app/pcba_test/S80_pcba_server \
+                    $(TARGET_DIR)/etc/init.d/
+endef
 endif
 
 ifeq ($(BR2_PACKAGE_PCBA_TEST_SELTTEST_APP),y)
