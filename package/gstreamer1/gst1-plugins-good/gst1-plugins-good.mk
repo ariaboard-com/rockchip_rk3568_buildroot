a20f3c1da33a0621918a9a829149440357921fa3
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-09-18 18:25:55 +0800

    gstreamer1: Bump plugins for webrtc
    Bump webrtc related gst plugins to upstream to support more features.
    
    Change-Id: I068452702a4d28a6c7d89314371f7a61b17dea73
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/gstreamer1/gst1-plugins-good/gst1-plugins-good.mk
+++ package/gstreamer1/gst1-plugins-good/gst1-plugins-good.mk
@@ -10,6 +10,9 @@ GST1_PLUGINS_GOOD_SITE = https://gstreamer.freedesktop.org/src/gst-plugins-good
 GST1_PLUGINS_GOOD_LICENSE_FILES = COPYING
 GST1_PLUGINS_GOOD_LICENSE = LGPL-2.1+
 
+GST1_PLUGINS_GOOD_AUTORECONF := YES
+GST1_PLUGINS_GOOD_GETTEXTIZE := YES
+
 GST1_PLUGINS_GOOD_CONF_OPTS = \
 	--disable-valgrind \
 	--disable-examples \
