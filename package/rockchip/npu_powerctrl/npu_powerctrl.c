0866059f48100b7727d27f6c21ddc679c3c5795a
Caesar Wang <wxt@rock-chips.com>
2019-11-05 09:16:58 +0800

    package/rockchip: update npu_powerctrl
    1. add version v1.1
    2. adjust npu power key control
    3. add pcie ctrl for multi camera board
    4. disconnect acm before the npu into sleep
        fix the acm usb probabilistic can not connect when exit sleep mode
    5. move the vdd_log off behind the vcc_1v8
        fix 3399pro auto reset due to TSADC work in unknow state by vdd_log off
    
    Change-Id: I6c07826ea4d3858aaccb49ead6165aa6f960121d
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- package/rockchip/npu_powerctrl/npu_powerctrl.c
+++ package/rockchip/npu_powerctrl/npu_powerctrl.c
@@ -8,16 +8,22 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "npu_powerctrl.h"
 
 #define LOG_TAG "NPU_POWER"
 
+#include <npu_powerctrl.h>
+
+#define VERSION "V1.1"
 #define FNAME_SIZE 50
 #define GPIO_BASE_PATH "/sys/class/gpio"
 #define GPIO_EXPORT_PATH GPIO_BASE_PATH "/export"
 #define GPIO_UNEXPORT_PATH GPIO_BASE_PATH "/unexport"
 #define CLKEN_24M_PATH "/sys/kernel/debug/clk/clk_wifi_pmu/clk_enable_count"
 #define CLKEN_32k_PATH "/sys/kernel/debug/clk/rk808-clkout2/clk_enable_count"
+#define PCIE_RESET_EP "sys/devices/platform/f8000000.pcie/pcie_reset_ep"
+#define ACM_HIGHSPEED_ID "/sys/bus/platform/devices/fe380000.usb/usb*/*/idProduct"
+#define ACM_FULLSPEED_ID "/sys/bus/platform/devices/fe3a0000.usb/usb*/*/idProduct"
+
 
 #define NPU_VDD_0V8_GPIO 	"4"  //GPIO0_PA4
 #define NPU_VDD_LOG_GPIO 	"10" //GPIO0_PB2
@@ -135,6 +141,7 @@ static int set_gpio(char *gpio_number, char *val) {
 void npu_power_gpio_init(void) {
 	int ret, index = 0, gpio_cnt = sizeof(gpio_list)/sizeof(int);
 
+	printf("version: %s\n", VERSION);
 	while (index != gpio_cnt) {
 		printf("init gpio: %s\n", gpio_list[index]);
 		ret = request_gpio(gpio_list[index]);
@@ -161,15 +168,12 @@ void npu_reset(void) {
 //	sysfs_write("/sys/power/wake_lock", "npu_lock");
 	sysfs_write(CLKEN_32k_PATH, "1");
 	clk_enable("0");
-	/*power off*/
-	set_gpio(NPU_VDD_LOG_GPIO, "0");
-	/* wait for usb disconnect */
-	usleep(2000);
 	set_gpio(NPU_VDD_GPIO, "0");
 	set_gpio(NPU_VCCIO_3V3_GPIO, "0");
 	set_gpio(NPU_VDD_CPU_GPIO, "0");
 	set_gpio(NPU_VCC_1V8_GPIO, "0");
 	set_gpio(NPU_VDD_0V8_GPIO, "0");
+	set_gpio(NPU_VDD_LOG_GPIO, "0");
 	set_gpio(CPU_INT_NPU_GPIO, "0");
 	set_gpio(CPU_RESET_NPU_GPIO, "0");
 	usleep(2000);
@@ -193,31 +197,75 @@ void npu_reset(void) {
 }
 
 void npu_poweroff(void) {
-	/*power off*/
-	set_gpio(NPU_VDD_LOG_GPIO, "0");
-	/* wait for usb disconnect */
-	usleep(2000);
 	set_gpio(NPU_VDD_GPIO, "0");
 	set_gpio(NPU_VCCIO_3V3_GPIO, "0");
 	set_gpio(NPU_VDD_CPU_GPIO, "0");
 	set_gpio(NPU_VCC_1V8_GPIO, "0");
 	set_gpio(NPU_VDD_0V8_GPIO, "0");
+	set_gpio(NPU_VDD_LOG_GPIO, "0");
 	set_gpio(CPU_INT_NPU_GPIO, "0");
 	set_gpio(CPU_RESET_NPU_GPIO, "0");
 	clk_enable("0");
 //	sysfs_write("/sys/power/wake_unlock", "npu_lock");
 }
 
+int disconnect_usb_acm(void)
+{
+	FILE *stream;
+	char buf[100];
+	int idx = 0, path_idx=0;
+
+	memset( buf,'\0',sizeof(buf) );
+	stream = popen( "cat " ACM_HIGHSPEED_ID, "r" );
+	if (!stream) {
+		path_idx=1;
+		stream = popen( "cat " ACM_FULLSPEED_ID, "r" );
+		if (!stream)
+			return -1;
+	}
+	fread( buf, sizeof(char), sizeof(buf), stream);
+	pclose( stream );
+	printf("ACM idProduct: %s", buf);
+	if (!strncmp(buf, "1005", 4)) {
+		if (path_idx)
+			stream = popen("find /sys/bus/platform/devices/fe3a0000.usb/usb*/*/ -name remove", "r" );
+		else
+			stream = popen("find /sys/bus/platform/devices/fe380000.usb/usb*/*/ -name remove", "r" );
+		fread( buf, sizeof(char), sizeof(buf), stream);
+		printf("usb remove patch is %s", buf);
+		/* The path string adds end character */
+		while(idx < 100) {
+			if (buf[idx] == '\n') {
+				buf[idx] = '\0';
+				break;
+			}
+			idx ++;
+		}
+		sysfs_write(buf, "0");
+		pclose( stream );
+		return 0;
+	}
+	return -1;
+
+}
+
 int npu_suspend(void) {
 	int retry=100;
+	int is_pcie;
 
 	if (get_gpio(NPU_PMU_SLEEP_GPIO)) {
 		printf("It is sleeping state, noting to do!\n");
 		return 0;
 	}
 
+	is_pcie = access(PCIE_RESET_EP, R_OK);
+	if (!is_pcie) {
+		sysfs_write(PCIE_RESET_EP, "2");
+		disconnect_usb_acm();
+	}
+
 	set_gpio(CPU_INT_NPU_GPIO, "1");
-	usleep(100000);
+	usleep(20000);
 	set_gpio(CPU_INT_NPU_GPIO, "0");
 
 	/*wait for npu enter sleep*/
@@ -244,6 +292,7 @@ int npu_suspend(void) {
 
 int npu_resume(void) {
 	int retry=100;
+	int is_pcie;
 
 	if (!get_gpio(NPU_PMU_SLEEP_GPIO)) {
 		printf("It is awakening state, noting to do!\n");
@@ -257,22 +306,29 @@ int npu_resume(void) {
 	usleep(10000);
 
 	set_gpio(CPU_INT_NPU_GPIO, "1");
+	usleep(20000);
+	set_gpio(CPU_INT_NPU_GPIO, "0");
 
 	/*wait for npu wakeup*/
 	while (--retry) {
 		if (!get_gpio(NPU_PMU_SLEEP_GPIO)) {
-	//		sysfs_write("/sys/power/wake_lock", "npu_lock");
+		//	sysfs_write("/sys/power/wake_lock", "npu_lock");
 			break;
 		}
 		usleep(10000);
 	}
+
+	is_pcie = access(PCIE_RESET_EP, R_OK);
+	if (!is_pcie)
+		sysfs_write(PCIE_RESET_EP, "1");
+
 	if (!retry) {
 		printf("npu resume timeout in one second\n");
 		return -1;
 	}
 	//waiting for userspase wakup
-	usleep(500000);
-	set_gpio(CPU_INT_NPU_GPIO, "0");
+	/*usleep(500000);*/
+	/*set_gpio(CPU_INT_NPU_GPIO, "0");*/
 
 	return 0;
 }
