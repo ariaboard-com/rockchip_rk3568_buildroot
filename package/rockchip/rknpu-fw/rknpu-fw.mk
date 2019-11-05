e91e5838d37b60b902cbc8d7b48567b7a8cc0d7b
Caesar Wang <wxt@rock-chips.com>
2019-11-05 17:35:51 +0800

    package/rockchip: update rknpu fw for pcie
    Change-Id: Ieea24977ea92e834e78e5def28a6023c63739320
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- package/rockchip/rknpu-fw/rknpu-fw.mk
+++ package/rockchip/rknpu-fw/rknpu-fw.mk
@@ -7,18 +7,24 @@ RKNPU_FW_VERSION = 1.0.0
 RKNPU_FW_SITE_METHOD = local
 RKNPU_FW_SITE = $(TOPDIR)/../external/rknpu-fw
 
-ifeq ($(BR2_PACKAGE_N4),y)
+ifeq ($(BR2_PACKAGE_RKNPU_PCIE),y)
 define RKNPU_FW_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/npu_fw
-    $(INSTALL) -m 0755 -D $(@D)/npu_fw_n4/* $(TARGET_DIR)/usr/share/npu_fw/
-    $(INSTALL) -m 0755 -D $(@D)/bin/* $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/npu_fw_pcie/* $(TARGET_DIR)/usr/share/npu_fw/
+    $(INSTALL) -m 0755 -D $(@D)/bin/npu-image.sh $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/bin/npu_transfer_proxy $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/bin/npu_upgrade_pcie $(TARGET_DIR)/usr/bin/npu_upgrade
+    $(INSTALL) -m 0755 -D $(@D)/bin/upgrade_tool $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 -D $(@D)/S11_npu_init $(TARGET_DIR)/etc/init.d/
 endef
 else
 define RKNPU_FW_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/npu_fw
-    $(INSTALL) -m 0755 -D $(@D)/npu_fw/* $(TARGET_DIR)/usr/share/npu_fw/
-    $(INSTALL) -m 0755 -D $(@D)/bin/* $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/npu_fw_n4/* $(TARGET_DIR)/usr/share/npu_fw/
+    $(INSTALL) -m 0755 -D $(@D)/bin/npu-image.sh $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/bin/npu_transfer_proxy $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/bin/npu_upgrade $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 -D $(@D)/bin/upgrade_tool $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 -D $(@D)/S11_npu_init $(TARGET_DIR)/etc/init.d/
 endef
 endif
