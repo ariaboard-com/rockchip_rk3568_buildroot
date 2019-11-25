e853a7b0d4239f25a8bb421c8b740e9e8cf99744
Frank Liu <frank.liu@rock-chips.com>
2019-11-25 19:55:02 +0800

    package: rv1108: add ogg_playback interface to adk.
    Change-Id: Ib2d6a1e0986056d5a5c65fde90c2a13a07921794
    Signed-off-by: Frank Liu <frank.liu@rock-chips.com>

--- package/rockchip/rv1108_package/framework/adk/adk.mk
+++ package/rockchip/rv1108_package/framework/adk/adk.mk
@@ -5,4 +5,9 @@ ADK_INSTALL_STAGING = YES
 # add dependencies
 ADK_DEPENDENCIES = libion
 
+ifeq ($(BR2_PACKAGE_ADK_MEDIA),y)
+    ADK_DEPENDENCIES += alsa-lib rkmedia
+    ADK_CONF_OPTS += -DENABLE_ADK_MEDIA=1
+endif
+
 $(eval $(cmake-package))
