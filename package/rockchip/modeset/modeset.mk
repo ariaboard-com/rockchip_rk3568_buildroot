a25e4525cc23c1b59a92393f21be42bc38ff5f2b
Yu YongZhen <yuyz@rock-chips.com>
2019-11-25 21:23:05 +0800

    modeset: add boot animation resources
    Change-Id: Ia5557cd68d2d67556c9f8c5ca51a21132b92aa20
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>

--- package/rockchip/modeset/modeset.mk
+++ package/rockchip/modeset/modeset.mk
@@ -5,4 +5,12 @@ MODESET_SITE_METHOD = local
 
 MODESET_CONF_DEPENDENCIES += libdrm jpeg
 
+define MODESET_COPY_BOOTANIMATION_RESOURCE
+	mkdir -p $(TARGET_DIR)/mnt/bootanimation
+	cp -rf  $(@D)/bootanimation $(TARGET_DIR)/mnt/
+	cp -f $(@D)/S01bootanimation $(TARGET_DIR)/etc/init.d/
+endef
+
+MODESET_POST_INSTALL_TARGET_HOOKS += MODESET_COPY_BOOTANIMATION_RESOURCE
+
 $(eval $(cmake-package))
