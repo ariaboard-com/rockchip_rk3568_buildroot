accbf33a464b9d52fb7020d63a24fda317d58b6f
Eddie Cai <eddie.cai.linux@gmail.com>
2019-11-26 14:08:43 +0800

    package: rockchip: deviceio_release: use binary for bcm and cypress bsa
    Change-Id: I4ab7510b907c246819aa129c3b2fbf4f009d2a05
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- package/rockchip/deviceio_release/deviceio_release.mk
+++ package/rockchip/deviceio_release/deviceio_release.mk
@@ -6,27 +6,40 @@
 DEVICEIO_RELEASE_SITE = $(TOPDIR)/../external/deviceio_release
 DEVICEIO_RELEASE_SITE_METHOD = local
 DEVICEIO_RELEASE_INSTALL_STAGING = YES
+DEVICEIO_RELEASE_DEPENDENCIES += wpa_supplicant alsa-lib
+BT_TTY_DEV = $(call qstrip,$(BR2_PACKAGE_RKWIFIBT_BTUART))
+ifeq ($(call qstrip,$(BR2_ARCH)), arm)
+        DEVICEIOARCH = lib32
+	BSAARCH = arm
+else ifeq ($(call qstrip, $(BR2_ARCH)), aarch64)
+        DEVICEIOARCH = lib64
+	BSAARCH = arm64
+endif
 
 ifeq ($(call qstrip,$(BR2_PACKAGE_RKWIFIBT_VENDOR)), REALTEK)
 	LIBDEVICEIOSO = libDeviceIo_bluez.so
 	DEVICEIO_RELEASE_DEPENDENCIES += readline bluez5_utils libglib2 bluez-alsa
 else ifeq ($(call qstrip,$(BR2_PACKAGE_RKWIFIBT_VENDOR)), BROADCOM)
 	LIBDEVICEIOSO = libDeviceIo_broadcom.so
-	DEVICEIO_RELEASE_DEPENDENCIES += broadcom_bsa
-else
+	DEVICEIO_BSA = broadcom_bsa
+else ifeq ($(call qstrip,$(BR2_PACKAGE_RKWIFIBT_VENDOR)), CYPRESS)
 	LIBDEVICEIOSO = libDeviceIo_cypress.so
-	DEVICEIO_RELEASE_DEPENDENCIES += cypress_bsa
-endif
-
-DEVICEIO_RELEASE_DEPENDENCIES += wpa_supplicant alsa-lib
-
-ifeq ($(call qstrip,$(BR2_ARCH)), arm)
-	DEVICEIOARCH = lib32
-else ifeq ($(call qstrip, $(BR2_ARCH)), aarch64)
-	DEVICEIOARCH = lib64
+	DEVICEIO_BSA = cypress_bsa
+else
+	LIBDEVICEIOSO = libDeviceIo_fake.so
+	DEVICEIO_BSA = fake
 endif
 
 define DEVICEIO_RELEASE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/$(DEVICEIO_BSA)/$(BSAARCH)/libbsa.so $(TARGET_DIR)/usr/lib/libbsa.so
+	$(INSTALL) -D -m 0755 $(@D)/$(DEVICEIO_BSA)/bsa_bt_sink.sh $(TARGET_DIR)/usr/bin/bsa_bt_sink.sh
+	$(INSTALL) -D -m 0755 $(@D)/$(DEVICEIO_BSA)/bsa_server.sh $(TARGET_DIR)/usr/bin/bsa_server.sh
+	$(INSTALL) -D -m 0755 $(@D)/$(DEVICEIO_BSA)/$(BSAARCH)/app_manager $(TARGET_DIR)/usr/bin/app_manager
+	$(INSTALL) -D -m 0755 $(@D)/$(DEVICEIO_BSA)/$(BSAARCH)/bsa_server $(TARGET_DIR)/usr/bin/bsa_server
+	$(INSTALL) -D -m 0755 $(@D)/$(DEVICEIO_BSA)/$(BSAARCH)/libbsa.so $(TARGET_DIR)/usr/lib/libbsa.so
+	sed -i 's/BTFIRMWARE_PATH/\/system\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_bt_sink.sh
+	sed -i 's/BTFIRMWARE_PATH/\/system\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_server.sh
+	sed -i 's/BT_TTY_DEV/\/dev\/$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_server.sh
 	$(INSTALL) -D -m 0755 $(@D)/DeviceIO/$(DEVICEIOARCH)/$(LIBDEVICEIOSO) $(TARGET_DIR)/usr/lib/libDeviceIo.so
 	$(INSTALL) -D -m 0755 $(@D)/DeviceIO/$(DEVICEIOARCH)/$(LIBDEVICEIOSO) $(STAGING_DIR)/usr/lib/libDeviceIo.so
 endef
