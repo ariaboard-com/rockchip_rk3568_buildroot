661af1721ae30001d6124deb916a579e92fcbcc6
zain wang <wzz@rock-chips.com>
2019-12-25 16:49:12 +0800

    package: python-empy: change setup_type to distutils
    Change-Id: Ie20dff06715762642ccb1829b2b1161bed250cd5
    Signed-off-by: zain wang <wzz@rock-chips.com>

--- package/rockchip/ros/python-ros/python-empy/python-empy.mk
+++ package/rockchip/ros/python-ros/python-empy/python-empy.mk
@@ -9,6 +9,6 @@ PYTHON_EMPY_SITE = http://www.alcyone.com/software/empy
 PYTHON_EMPY_SOURCE = empy-$(PYTHON_EMPY_VERSION).tar.gz
 PYTHON_EMPY_LICENSE = LGPL-3.0
 PYTHON_EMPY_LICENSE_FILES = LICENSE.txt
-PYTHON_EMPY_SETUP_TYPE = setuptools
+PYTHON_EMPY_SETUP_TYPE = distutils
 
 $(eval $(host-python-package))
