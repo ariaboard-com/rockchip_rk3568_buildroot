8bc6d7be4e352f9bcbe005163376c4bfd0c2e346
Caesar Wang <wxt@rock-chips.com>
2020-01-10 19:06:55 +0800

    package/rockchip: add npu_fw_pvtm-32k to support
    Change-Id: I5be8f7c7a0383b46db9becb0fe77d271f15badcf
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- package/rockchip/rknpu-fw/rknpu-fw.mk
+++ package/rockchip/rknpu-fw/rknpu-fw.mk
@@ -7,10 +7,22 @@ RKNPU_FW_VERSION = 1.0.0
 RKNPU_FW_SITE_METHOD = local
 RKNPU_FW_SITE = $(TOPDIR)/../external/rknpu-fw
 
+ifeq ($(BR2_PACKAGE_NPU_POWERCTRL_COMBINE),y)
+RKNPU_FW_PCIE_TYPE = npu_fw_pvtm-32k/pcie/*
+else
+RKNPU_FW_PCIE_TYPE = npu_fw_pcie/*
+endif
+
+ifeq ($(BR2_PACKAGE_NPU_POWERCTRL_COMBINE),y)
+RKNPU_FW_USB_TYPE = npu_fw_pvtm-32k/usb/*
+else
+RKNPU_FW_USB_TYPE = npu_fw/*
+endif
+
 ifeq ($(BR2_PACKAGE_RKNPU_PCIE),y)
 define RKNPU_FW_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/npu_fw
-    $(INSTALL) -m 0755 -D $(@D)/npu_fw_pcie/* $(TARGET_DIR)/usr/share/npu_fw/
+    $(INSTALL) -m 0755 -D $(@D)/$(RKNPU_FW_PCIE_TYPE) $(TARGET_DIR)/usr/share/npu_fw/
     $(INSTALL) -m 0755 -D $(@D)/bin/npu-image.sh $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 -D $(@D)/bin/npu_transfer_proxy $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 -D $(@D)/bin/npu_upgrade_pcie $(TARGET_DIR)/usr/bin/npu_upgrade
@@ -20,7 +32,7 @@ endef
 else
 define RKNPU_FW_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/npu_fw
-    $(INSTALL) -m 0755 -D $(@D)/npu_fw/* $(TARGET_DIR)/usr/share/npu_fw/
+    $(INSTALL) -m 0755 -D $(@D)/$(RKNPU_FW_USB_TYOE) $(TARGET_DIR)/usr/share/npu_fw/
     $(INSTALL) -m 0755 -D $(@D)/bin/npu-image.sh $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 -D $(@D)/bin/npu_transfer_proxy $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 -D $(@D)/bin/npu_upgrade $(TARGET_DIR)/usr/bin/
