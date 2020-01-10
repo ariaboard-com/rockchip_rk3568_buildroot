130556cb0de5883a62b71e9d4eb7a0e3418b9179
Zhichao Yu <zhichao.yu@rock-chips.com>
2020-01-10 15:54:33 +0800

    configs/rv1108/cvr: use device_manager to replace udev
    Change-Id: I3fa66db73933394826c75bedb90cdf61f1a0127b
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>

--- package/rockchip/rv1108_package/app/cvr/cvr.mk
+++ package/rockchip/rv1108_package/app/cvr/cvr.mk
@@ -8,11 +8,10 @@ CVR_DEPENDENCIES = rkcamera messenger process_units libpng12 rv1108_minigui rknr
 CVR_CONF_OPTS += -DUI_RESOLUTION=$(call qstrip,$(RK_UI_RESOLUTION))
 
 define CVR_INSTALL_INIT_SYSV
-	$(INSTALL) -m 0755 -D package/rockchip/rv1108_package/app/cvr/S12_cvr \
-		    $(TARGET_DIR)/etc/init.d
-
-    $(INSTALL) -m 0644 -D package/rockchip/rv1108_package/app/cvr/cvr.conf \
-                    $(TARGET_DIR)/etc/cvr.conf
+	[ -z "$(BR2_PACKAGE_SYSTEM_MANAGER)" ] && $(INSTALL) -m 0755 -D \
+			package/rockchip/rv1108_package/app/cvr/S12_cvr $(TARGET_DIR)/etc/init.d || true
+	$(INSTALL) -m 0644 -D package/rockchip/rv1108_package/app/cvr/cvr.conf \
+			$(TARGET_DIR)/etc/cvr.conf
 endef
 
 $(eval $(cmake-package))
