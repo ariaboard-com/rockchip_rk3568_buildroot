b7204a43e199ff54b39b852fca7f9b4246a043d1
Randall Zhuo <randall.zhuo@rock-chips.com>
2020-01-13 14:28:22 +0800

    1. add rk1806 32bit support 2. move npu ko install dir from /usr/lib/modules/ to /lib/modules/
    Change-Id: I6621d3217fa0e9d45f919b3940767bc7738014d7
    Signed-off-by: Randall Zhuo <randall.zhuo@rock-chips.com>
    (cherry picked from commit acad6c1543052de5f88a123c82e22f4edadf00a8)
    Signed-off-by: Lin Jianhua <linjh@rock-chips.com>

--- package/rockchip/rknpu/rknpu.mk
+++ package/rockchip/rknpu/rknpu.mk
@@ -10,13 +10,18 @@ NPU_TEST_FILE = $(@D)/test
 
 ifeq ($(BR2_PACKAGE_RKNPU_PCIE),y)
 NPU_KO_FILE = galcore_rk3399pro-npu-pcie.ko
-NPU_INIT_FILE = S99NPU_init
 else ifeq ($(BR2_PACKAGE_RK3399PRO_NPU),y)
 NPU_KO_FILE = galcore_rk3399pro-npu.ko
-NPU_INIT_FILE = S99NPU_init
+else ifeq ($(BR2_PACKAGE_RK1806),y)
+NPU_KO_FILE = galcore_rk1806.ko
 else
 NPU_KO_FILE = galcore.ko
-NPU_INIT_FILE = S99NPU_init
+endif
+
+ifeq ($(BR2_arm),y)
+NPU_PLATFORM = linux-armhf
+else
+NPU_PLATFORM = linux-aarch64
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_RKNN), y)
@@ -24,18 +29,16 @@ BUILD_PYTHON_RKNN=y
 endif
 
 define RKNPU_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/lib/modules/
+    mkdir -p $(TARGET_DIR)/lib/modules/
     mkdir -p $(TARGET_DIR)/usr/share/npu/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/$(NPU_KO_FILE) $(TARGET_DIR)/usr/lib/modules/galcore.ko
-    $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(TARGET_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/rknn/usr/lib/*.so $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/cl_*.h $(TARGET_DIR)/usr/lib/
-    $(INSTALL) -D -m 0755 $(@D)/$(NPU_INIT_FILE) $(TARGET_DIR)/etc/init.d/S99NPU_init
-    cp -r $(@D)/rknn/usr/* $(TARGET_DIR)/usr/
+    $(INSTALL) -D -m 0644 $(@D)/drivers/npu_ko/$(NPU_KO_FILE) $(TARGET_DIR)/lib/modules/galcore.ko
+    cp -r $(@D)/drivers/common/* $(TARGET_DIR)/
+    cp -r $(@D)/drivers/common/* $(STAGING_DIR)/
+    cp -r $(@D)/drivers/$(NPU_PLATFORM)/* $(TARGET_DIR)/
+    cp -r $(@D)/drivers/$(NPU_PLATFORM)/* $(STAGING_DIR)/
 
     if [ -e "$(@D)/test" ]; then \
-	cp -r $(@D)/test $(TARGET_DIR)/usr/share/npu; \
+        cp -r $(@D)/test $(TARGET_DIR)/usr/share/npu; \
     fi
 
     if [ x${BUILD_PYTHON_RKNN} != x ]; then \
