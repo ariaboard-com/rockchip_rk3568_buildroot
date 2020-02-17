e354300b5fa0767fde2fbe4a59f5867c341b5c03
vicent.chi <vicent.chi@rock-chips.com>
2020-02-17 16:01:10 +0800

    package/rockchip/mediaserver: add configurable options
    Change-Id: I16a55cd1230eeb29ae4dcba3591610c8579d38ac
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

--- package/rockchip/mediaserver/mediaserver.mk
+++ package/rockchip/mediaserver/mediaserver.mk
@@ -5,4 +5,9 @@ MEDIASERVER_DEPENDENCIES = rkmedia dbus dbus-cpp librkdb json-for-modern-cpp
 
 MEDIASERVER_CONF_OPTS += -DBR2_SDK_PATH=$(HOST_DIR)
 
+ifeq ($(BR2_PACKAGE_MEDIASERVE_LINKAPI), y)
+    MEDIASERVER_CONF_OPTS += -DENABLE_LINK_SUPPORT=ON
+    MEDIASERVER_DEPENDENCIES += cjson
+endif
+
 $(eval $(cmake-package))
