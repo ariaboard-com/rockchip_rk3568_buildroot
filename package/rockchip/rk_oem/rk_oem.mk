c1f2f35faffe6ac1017ecf7ad5cd031a0a81e38e
Weiwen Chen <cww@rock-chips.com>
2020-03-03 10:18:26 +0800

    package: rockchip: add rk_oem
    Change-Id: I9c713ee5a00e5f0dfdfe4cdd9040bc9574ea02ad
    Signed-off-by: Weiwen Chen <cww@rock-chips.com>

---
+++ package/rockchip/rk_oem/rk_oem.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# rk_oem
+#
+################################################################################
+
+ifeq ($(BR2_PACKAGE_RK_OEM), y)
+RK_OEM_SITE_METHOD = local
+
+ifeq ($(BR2_PACKAGE_RK_OEM_RESOURCE_DIR),"")
+RK_OEM_SITE = $(TOPDIR)/package/rockchip/rk_oem/src
+
+define RK_OEM_INSTALL_INIT_SYSV
+$(INSTALL) -D -m 0755 $(@D)/S98_lunch_init \
+		$(TARGET_DIR)/etc/init.d/S98_lunch_init
+endef
+
+else
+RK_OEM_SITE = $(BR2_PACKAGE_RK_OEM_RESOURCE_DIR)
+endif
+
+RK_OEM_INSTALL_TARGET_DIR = $(BR2_PACKAGE_RK_OEM_INSTALL_TARGET_DIR)
+
+define RK_OEM_INSTALL_TARGET_CMDS
+	mkdir -p $(RK_OEM_INSTALL_TARGET_DIR)
+	cp -rfp $(@D)/* $(RK_OEM_INSTALL_TARGET_DIR)
+	rm -fv $(RK_OEM_INSTALL_TARGET_DIR)/rk_oem.tar
+endef
+
+endif
+
+$(eval $(generic-package))
