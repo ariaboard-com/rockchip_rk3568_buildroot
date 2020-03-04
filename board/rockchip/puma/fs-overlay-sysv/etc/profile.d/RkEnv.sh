8d5dc219bb59bf385815e585975e0776b466d309
Weiwen Chen <cww@rock-chips.com>
2020-03-04 19:44:12 +0800

    puma: fs-overlay-sysv: RkEnv.sh: add oem PATH and LD_LIBRARY_PATH
    Change-Id: I1de57c7f3cab05b6e45bd3e712e8e318ab30980d
    Signed-off-by: Weiwen Chen <cww@rock-chips.com>

--- board/rockchip/puma/fs-overlay-sysv/etc/profile.d/RkEnv.sh
+++ board/rockchip/puma/fs-overlay-sysv/etc/profile.d/RkEnv.sh
@@ -1,4 +1,4 @@
 export HOME=/oem
-export PATH=$PATH:$HOME:$HOME/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usrlibs
+export PATH=$PATH:$HOME:$HOME/bin:$HOME/usr/bin:$HOME/sbin:$HOME/usr/sbin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usrlibs:$HOME/usr/lib:$HOME/lib
 
