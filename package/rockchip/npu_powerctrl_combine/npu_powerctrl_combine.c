edaf240e56b6fbbfa4251f32d258ce19a34c7851
Caesar Wang <wxt@rock-chips.com>
2020-03-04 18:24:32 +0800

    package/rockchip: fixes the npu_powerctrl_combine
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I78cf712bb5ff7e7605e1eb08e49af1e4c494ae2b

--- package/rockchip/npu_powerctrl_combine/npu_powerctrl_combine.c
+++ package/rockchip/npu_powerctrl_combine/npu_powerctrl_combine.c
@@ -18,9 +18,9 @@
 #define GPIO_UNEXPORT_PATH GPIO_BASE_PATH "/unexport"
 #define CLKEN_24M_PATH "/sys/kernel/debug/clk/clk_wifi_pmu/clk_enable_count"
 #define CLKEN_32k_PATH "/sys/kernel/debug/clk/rk808-clkout2/clk_enable_count"
-#define PCIE_RESET_EP "sys/devices/platform/f8000000.pcie/pcie_reset_ep"
-#define ACM_HIGHSPEED_ID "/sys/bus/platform/devices/fe380000.usb/usb*/idProduct"
-#define ACM_FULLSPEED_ID "/sys/bus/platform/devices/fe3a0000.usb/usb*/idProduct"
+#define PCIE_RESET_EP "/sys/devices/platform/f8000000.pcie/pcie_reset_ep"
+#define ACM_HIGHSPEED_ID "/sys/bus/platform/devices/fe380000.usb/usb*/*/idProduct"
+#define ACM_FULLSPEED_ID "/sys/bus/platform/devices/fe3a0000.usb/usb*/*/idProduct"
 
 #define CPU_RESET_NPU_GPIO 	"32" //GPIO1_PA0
 #define NPU_PMU_SLEEP_GPIO 	"35" //GPIO1_A3
