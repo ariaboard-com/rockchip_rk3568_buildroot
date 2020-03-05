d900022b8faa5d402bcfd281eac75d34858b0d21
Weiwen Chen <cww@rock-chips.com>
2020-03-05 09:30:43 +0800

    package: rockchip: rk_oem: support include dbus conf dir
    Change-Id: Icceda94a9084f4ec7068cba18959344f9f2f7738
    Signed-off-by: Weiwen Chen <cww@rock-chips.com>

--- package/rockchip/rk_oem/rk_oem.mk
+++ package/rockchip/rk_oem/rk_oem.mk
@@ -19,9 +19,13 @@ else
 RK_OEM_SITE = $(BR2_PACKAGE_RK_OEM_RESOURCE_DIR)
 endif
 
+RK_OEM_REDIRECT_DBUS4OEM_CONF = package/rockchip/rk_oem/redirect_dbus4oem.conf
 RK_OEM_INSTALL_TARGET_DIR = $(BR2_PACKAGE_RK_OEM_INSTALL_TARGET_DIR)
 
 define RK_OEM_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/dbus-1/system.d
+	$(INSTALL) -D -m 0644 $(RK_OEM_REDIRECT_DBUS4OEM_CONF) \
+		$(TARGET_DIR)/usr/share/dbus-1/system.d
 	mkdir -p $(RK_OEM_INSTALL_TARGET_DIR)
 	cp -rfp $(@D)/* $(RK_OEM_INSTALL_TARGET_DIR)
 	rm -fv $(RK_OEM_INSTALL_TARGET_DIR)/rk_oem.tar
