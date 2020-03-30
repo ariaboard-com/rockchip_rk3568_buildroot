793ca0b6bd97b07047afd46a9dce62a6ded6cc57
Herman Chen <herman.chen@rock-chips.com>
2020-03-30 18:41:13 +0800

    package: rockchip: mpp: Fix verion missing issue
    Add POST_RSYNC hookd for soft link to source git repo. Then mpp cmake
    script can generate correct version log.
    
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>
    Change-Id: I615440c2a311f78006f003f8bb9090582e8c96d0

--- package/rockchip/mpp/mpp.mk
+++ package/rockchip/mpp/mpp.mk
@@ -12,6 +12,13 @@ ifeq ($(BR2_PACKAGE_MPP_ALLOCATOR_DRM),y)
 MPP_CONF_OPTS += "-DHAVE_DRM=ON"
 endif
 
+define MPP_LINK_GIT
+	rm -rf $(@D)/.git
+	ln -s $(SRCDIR)/.git $(@D)/
+endef
+
+MPP_POST_RSYNC_HOOKS += MPP_LINK_GIT
+
 ifeq ($(BR2_PACKAGE_RK3328),y)
 define MPP_H265_SUPPORTED_FIRMWARE
 	mkdir -p $(TARGET_DIR)/lib/firmware/
