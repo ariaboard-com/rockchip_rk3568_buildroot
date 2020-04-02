76474d9c1a8fbfb4dc1cb4438a2d49e68be863b8
Weiwen Chen <cww@rock-chips.com>
2020-04-02 19:49:50 +0800

    package: rockchip_test: fix npu test if chip support npu
    Signed-off-by: Weiwen Chen <cww@rock-chips.com>
    Change-Id: I732e7830b34e13e61dd48ae60b808750e942bcd3

--- package/rockchip/rockchip_test/rockchip_test.mk
+++ package/rockchip/rockchip_test/rockchip_test.mk
@@ -7,19 +7,18 @@ ROCKCHIP_TEST_SITE = $(TOPDIR)/package/rockchip/rockchip_test/src
 ROCKCHIP_TEST_LICENSE = Apache V2.0
 ROCKCHIP_TEST_LICENSE_FILES = NOTICE
 
-ifeq ($(BR2_PACKAGE_RK1808),y)
-define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
-	cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
-	cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
-	cp -rf $(@D)/npu ${TARGET_DIR}/rockchip_test/
-	$(INSTALL) -D -m 0755 $(@D)/rockchip_test/auto_reboot/S99_auto_reboot $(TARGET_DIR)/etc/init.d/
+ifeq ($(BR2_PACKAGE_RKNPU),y)
+define ROCKCHIP_TEST_INSTALL_NPU_TARGET_CMDS
+	rm -rf ${TARGET_DIR}/rockchip_test/npu
+	cp -rf $(@D)/npu_${ARCH}  ${TARGET_DIR}/rockchip_test/npu
 endef
-else
-define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
-	cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
-	cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
-	$(INSTALL) -D -m 0755 $(@D)/rockchip_test/auto_reboot/S99_auto_reboot $(TARGET_DIR)/etc/init.d/
-endef
+ROCKCHIP_TEST_POST_INSTALL_TARGET_HOOKS = ROCKCHIP_TEST_INSTALL_NPU_TARGET_CMDS
 endif
+
+define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
+	cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
+	cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
+	$(INSTALL) -D -m 0755 $(@D)/rockchip_test/auto_reboot/S99_auto_reboot $(TARGET_DIR)/etc/init.d/
+endef
 
 $(eval $(generic-package))
