87492b77ed50957de4c0625e4ba5577654420f20
Zhihua Wang <hogan.wang@rock-chips.com>
2020-04-07 19:14:19 +0800

    package: rockchip: ficial_gate: add USE_WEB_SERVER
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: Ie26a89614b28ae68cecf6c61a2e19738e596d364

--- package/rockchip/ficial_gate/ficial_gate.mk
+++ package/rockchip/ficial_gate/ficial_gate.mk
@@ -4,5 +4,9 @@ ifeq ($(BR2_PACKAGE_FICIAL_GATE), y)
     FICIAL_GATE_INSTALL_STAGING = YES
     FICIAL_GATE_DEPENDENCIES = rockface libdrm linux-rga tslib minigui jpeg libpng12 freetype sqlite camera_engine_rkisp alsa-lib mpp
     FICIAL_GATE_CONF_OPTS += "-DDRM_HEADER_DIR=$(STAGING_DIR)/usr/include/drm"
+    ifeq ($(BR2_PACKAGE_FICIAL_GATE_USE_WEB_SERVER), y)
+        FICIAL_GATE_CONF_OPTS += "-DUSE_WEB_SERVER=y"
+        FICIAL_GATE_DEPENDENCIES += libIPCProtocol libgdbus
+    endif
     $(eval $(cmake-package))
 endif
