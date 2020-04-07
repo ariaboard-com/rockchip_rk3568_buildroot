f4658567cfcee43997369b2c4fd7e1da5c677b1f
Zhihua Wang <hogan.wang@rock-chips.com>
2020-04-07 19:11:16 +0800

    package: rockchip: ficial_gate: select mpp for USB camera
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: I32f29b5a4a4aaac5ffb55cf574ffffe597063d64

--- package/rockchip/ficial_gate/ficial_gate.mk
+++ package/rockchip/ficial_gate/ficial_gate.mk
@@ -2,7 +2,7 @@ ifeq ($(BR2_PACKAGE_FICIAL_GATE), y)
     FICIAL_GATE_SITE = $(TOPDIR)/../app/ficial_gate
     FICIAL_GATE_SITE_METHOD = local
     FICIAL_GATE_INSTALL_STAGING = YES
-    FICIAL_GATE_DEPENDENCIES = rockface libdrm linux-rga tslib minigui jpeg libpng12 freetype sqlite camera_engine_rkisp alsa-lib
+    FICIAL_GATE_DEPENDENCIES = rockface libdrm linux-rga tslib minigui jpeg libpng12 freetype sqlite camera_engine_rkisp alsa-lib mpp
     FICIAL_GATE_CONF_OPTS += "-DDRM_HEADER_DIR=$(STAGING_DIR)/usr/include/drm"
     $(eval $(cmake-package))
 endif
