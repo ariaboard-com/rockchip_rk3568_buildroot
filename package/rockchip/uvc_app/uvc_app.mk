5f6974ee3446972d1a4f833b8e7d53ab4f424d9c
huangjc <huangjc@rock-chips.com>
2020-04-09 16:33:49 +0800

    package: uvc_app: select BR2_PACKAGE_RKMEDIA
    Change-Id: I9a9dcf1c7f5151c6f4065ef5d0ae7186ceec58dc
    Signed-off-by: huangjc <huangjc@rock-chips.com>

--- package/rockchip/uvc_app/uvc_app.mk
+++ package/rockchip/uvc_app/uvc_app.mk
@@ -3,5 +3,9 @@ ifeq ($(BR2_PACKAGE_UVC_APP), y)
     UVC_APP_SITE_METHOD = local
     UVC_APP_INSTALL_STAGING = YES
     UVC_APP_DEPENDENCIES = libdrm mpp
+ifneq ($(BR2_PACKAGE_RKMEDIA_UVC), y)
+    UVC_APP_DEPENDENCIES = rkmedia
+    UVC_APP_CONF_OPTS = -DCOMPILES_CAMERA=ON
+endif
     $(eval $(cmake-package))
 endif
