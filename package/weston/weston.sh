08e149f6c08fdfdc6ded32fc364bd38dbeffadba
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-05-13 16:18:19 +0800

    weston: Set related env variables in profile
    Change-Id: I60f8d56f7238f31af18d272d3a9d4b10fe184df4
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ package/weston/weston.sh
@@ -0,0 +1,21 @@
+# The env variables below can be override by the init script(e.g. S50launcher)
+
+if [ -d /tmp/.xdg ]; then
+	# Compatible with old S50launcher
+	export XDG_RUNTIME_DIR=/tmp/.xdg
+else
+	export XDG_RUNTIME_DIR=/var/run
+fi
+
+# Comment out these to disable mirror mode
+export WESTON_DRM_MIRROR=1
+export WESTON_DRM_KEEP_RATIO=1
+
+export QT_QPA_PLATFORM=wayland
+
+# The kmssink has better performance but is more limited.
+# export PREFERED_VIDEOSINK=kmssink
+export PREFERED_VIDEOSINK=waylandsink
+
+export QT_GSTREAMER_WIDGET_VIDEOSINK=${PREFERED_VIDEOSINK}
+export QT_GSTREAMER_WINDOW_VIDEOSINK=${PREFERED_VIDEOSINK}
