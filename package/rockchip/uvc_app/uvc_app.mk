50341adec0e5518aeb2da7be379bda3b2dd937d3
Zhihua Wang <hogan.wang@rock-chips.com>
2020-05-14 14:02:47 +0800

    package: rockchip: uvc_app: select camera_engine_rkisp
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: Ie29eed029ee0c828a36524df9760576673503f40

--- package/rockchip/uvc_app/uvc_app.mk
+++ package/rockchip/uvc_app/uvc_app.mk
@@ -2,7 +2,7 @@ ifeq ($(BR2_PACKAGE_UVC_APP), y)
     UVC_APP_SITE = $(TOPDIR)/../external/uvc_app
     UVC_APP_SITE_METHOD = local
     UVC_APP_INSTALL_STAGING = YES
-    UVC_APP_DEPENDENCIES = libdrm mpp
+    UVC_APP_DEPENDENCIES = libdrm mpp camera_engine_rkisp
 ifneq ($(BR2_PACKAGE_RKMEDIA_UVC), y)
     UVC_APP_DEPENDENCIES = rkmedia
     UVC_APP_CONF_OPTS = -DCOMPILES_CAMERA=ON
