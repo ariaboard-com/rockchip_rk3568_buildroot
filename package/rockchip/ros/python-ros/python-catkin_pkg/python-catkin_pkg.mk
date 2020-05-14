d9f84d5e9392fc54519d32b32657ae29c82cf79b
zain wang <wzz@rock-chips.com>
2020-05-14 14:17:19 +0800

    ros: python-catkin_pkg: add dependencies
    Signed-off-by: zain wang <wzz@rock-chips.com>
    Change-Id: I12833c99b1004202d61256e07e9a9bb65a92c4cf

--- package/rockchip/ros/python-ros/python-catkin_pkg/python-catkin_pkg.mk
+++ package/rockchip/ros/python-ros/python-catkin_pkg/python-catkin_pkg.mk
@@ -3,5 +3,7 @@ PYTHON_CATKIN_PKG_SOURCE = catkin_pkg-$(PYTHON_CATKIN_PKG_VERSION).tar.gz
 PYTHON_CATKIN_PKG_SITE = https://files.pythonhosted.org/packages/fa/e9/6977c1cd0ae22ca164dfed9007439ad89fc81d759e79286ebd024d48883b
 PYTHON_CATKIN_PKG_SETUP_TYPE = setuptools
 
+HOST_PYTHON_CATKIN_PKG_DEPENDENCIES = host-python-pyparsing host-python-dateutil
+
 $(eval $(host-python-package))
 $(eval $(python-package))
