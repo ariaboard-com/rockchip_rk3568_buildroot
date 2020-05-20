7e07ca33e3406d1d99986d451fbe447f07e3667f
Zhihua Wang <hogan.wang@rock-chips.com>
2020-05-20 11:37:45 +0800

    package: rockchip: ficial_gate: rkfacial depend
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: Ifb177c13975d6071fa89252b8f1141a1fc3f2dc0

--- package/rockchip/ficial_gate/ficial_gate.mk
+++ package/rockchip/ficial_gate/ficial_gate.mk
@@ -2,11 +2,7 @@ ifeq ($(BR2_PACKAGE_FICIAL_GATE), y)
     FICIAL_GATE_SITE = $(TOPDIR)/../app/ficial_gate
     FICIAL_GATE_SITE_METHOD = local
     FICIAL_GATE_INSTALL_STAGING = YES
-    FICIAL_GATE_DEPENDENCIES = rockface libdrm linux-rga tslib minigui jpeg libpng12 freetype sqlite camera_engine_rkisp alsa-lib mpp
+    FICIAL_GATE_DEPENDENCIES = rkfacial libdrm linux-rga tslib minigui jpeg libpng12 freetype
     FICIAL_GATE_CONF_OPTS += "-DDRM_HEADER_DIR=$(STAGING_DIR)/usr/include/drm"
-    ifeq ($(BR2_PACKAGE_FICIAL_GATE_USE_WEB_SERVER), y)
-        FICIAL_GATE_CONF_OPTS += "-DUSE_WEB_SERVER=y"
-        FICIAL_GATE_DEPENDENCIES += libIPCProtocol libgdbus
-    endif
     $(eval $(cmake-package))
 endif
