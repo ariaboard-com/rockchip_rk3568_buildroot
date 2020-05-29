4c7f7d74376fe0d718bfb446333f8b06af5e7e04
ctf <ctf@rock-chips.com>
2020-05-29 18:40:01 +0800

    package: rockchip: add QFacialGate
    Signed-off-by: ctf <ctf@rock-chips.com>
    Change-Id: I549f42b787bb194268de0471c69b2e9c4cc70c5c

---
+++ package/rockchip/QFacialGate/QFacialGate.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# QFacialGate
+#
+################################################################################
+
+QFACIALGATE_VERSION = 1.0
+QFACIALGATE_SITE = $(TOPDIR)/../app/QFacialGate
+QFACIALGATE_SITE_METHOD = local
+
+define QFACIALGATE_CONFIGURE_CMDS
+	cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/qmake
+endef
+
+define QFACIALGATE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QFACIALGATE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/QFacialGate	$(TARGET_DIR)/usr/bin/QFacialGate
+	$(INSTALL) -D -m 0755 $(@D)/S06_QFacialGate $(TARGET_DIR)/etc/init.d/
+endef
+
+$(eval $(generic-package))
