489af1586ee24546b741614641579ac1c2721a44
Zhihua Wang <hogan.wang@rock-chips.com>
2020-05-30 14:51:34 +0800

    package: rockchip: rkfacial: modify camera engine depends
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: Id606228186c99373ff6b9ba7879cdf69b87151ac

--- package/rockchip/rkfacial/rkfacial.mk
+++ package/rockchip/rkfacial/rkfacial.mk
@@ -2,7 +2,19 @@ ifeq ($(BR2_PACKAGE_RKFACIAL), y)
     RKFACIAL_SITE = $(TOPDIR)/../external/rkfacial
     RKFACIAL_SITE_METHOD = local
     RKFACIAL_INSTALL_STAGING = YES
-    RKFACIAL_DEPENDENCIES = rockface libdrm linux-rga sqlite camera_engine_rkisp alsa-lib mpp
+    RKFACIAL_DEPENDENCIES = rockface libdrm linux-rga sqlite alsa-lib mpp
+ifeq ($(BR2_PACKAGE_RK1806),y)
+    RKFACIAL_DEPENDENCIES += camera_engine_rkisp
+    RKFACIAL_CONF_OPTS += "-DCAMERA_ENGINE_RKISP=y"
+endif
+ifeq ($(BR2_PACKAGE_RK1808),y)
+    RKFACIAL_DEPENDENCIES += camera_engine_rkisp
+    RKFACIAL_CONF_OPTS += "-DCAMERA_ENGINE_RKISP=y"
+endif
+ifeq ($(BR2_PACKAGE_RV1126_RV1109),y)
+    RKFACIAL_DEPENDENCIES += camera_engine_rkaiq
+    RKFACIAL_CONF_OPTS += "-DCAMERA_ENGINE_RKAIQ=y"
+endif
     RKFACIAL_CONF_OPTS += "-DDRM_HEADER_DIR=$(STAGING_DIR)/usr/include/drm"
     ifeq ($(BR2_PACKAGE_RKFACIAL_USE_WEB_SERVER), y)
         RKFACIAL_CONF_OPTS += "-DUSE_WEB_SERVER=y"
