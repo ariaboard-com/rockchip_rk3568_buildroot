1d5aa3c9e491ab30a3fc97a5627c99765e6fee3c
lqh <kevin.lin@rock-chips.com>
2020-06-03 15:23:02 +0800

    uvc_app : add eptz supprot with rockx
    Signed-off-by: lqh <kevin.lin@rock-chips.com>
    Change-Id: I5de8bd01157e4833ae06b69ec28c06255df933a5

--- package/rockchip/uvc_app/uvc_app.mk
+++ package/rockchip/uvc_app/uvc_app.mk
@@ -6,9 +6,16 @@ ifeq ($(BR2_PACKAGE_UVC_APP), y)
 ifeq ($(BR2_PACKAGE_CAMERA_ENGINE_RKISP),y)
     UVC_APP_DEPENDENCIES += camera_engine_rkisp
 endif
+
 ifneq ($(BR2_PACKAGE_RKMEDIA_UVC), y)
     UVC_APP_DEPENDENCIES = rkmedia
     UVC_APP_CONF_OPTS = -DCOMPILES_CAMERA=ON
 endif
+
+ifeq ($(BR2_PACKAGE_ROCKX),y)
+    UVC_APP_DEPENDENCIES += rockx linux-rga
+    UVC_APP_CONF_OPTS += "-DEPTZ_SUPPORT=ON" "-DROCKX_HEADER_DIR=$(STAGING_DIR)/usr/include/rockx"
+endif
+
     $(eval $(cmake-package))
 endif
