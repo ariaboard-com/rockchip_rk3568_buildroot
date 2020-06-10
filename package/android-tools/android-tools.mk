2cc047750c9a8b12813925108896a2bba468e385
Ziyuan Xu <xzy.xu@rock-chips.com>
2020-06-10 09:54:12 +0800

    package: android-tools: support rsa authentication via env dynamically
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>
    Change-Id: I492bc02fdd31e9cf5da9b51503a5cf07f6dc6e82

--- package/android-tools/android-tools.mk
+++ package/android-tools/android-tools.mk
@@ -53,6 +53,14 @@ endif
 ifeq ($(BR2_PACKAGE_ANDROID_TOOLS_ADBD),y)
 ANDROID_TOOLS_TARGETS += adbd
 ANDROID_TOOLS_DEPENDENCIES += zlib openssl
+ifeq ($(BR2_PACKAGE_ANDROID_TOOLS_AUTH_RSA),y)
+ADBD_RSA_KEY_FILEPATH = $(call qstrip,$(BR2_PACKAGE_ANDROID_TOOLS_AUTH_RSA_KEY_PATH))
+define ANDROID_TOOLS_INSTALL_RSAAUTH_ENV
+	echo "export ADBD_RSA_AUTH_ENABLE=1" > $(TARGET_DIR)/etc/profile.d/adbd.sh
+	echo "export ADBD_RSA_KEY_FILE=${ADBD_RSA_KEY_FILEPATH}" >> $(TARGET_DIR)/etc/profile.d/adbd.sh
+	$(INSTALL) -D -m 0644 $(HOME)/.android/adbkey.pub $(TARGET_DIR)/${ADBD_RSA_KEY_FILEPATH}
+endef
+endif
 endif
 
 # Build each tool in its own directory not to share object files
@@ -90,6 +98,7 @@ define ANDROID_TOOLS_INSTALL_TARGET_CMDS
 	$(foreach t,$(ANDROID_TOOLS_TARGETS),\
 		$(INSTALL) -D -m 0755 $(@D)/build-$(t)/$(t) $(TARGET_DIR)/usr/bin/$(t)$(sep))
 	$(ANDROID_TOOLS_INSTALL_AUTH)
+	$(ANDROID_TOOLS_INSTALL_RSAAUTH_ENV)
 endef
 
 $(eval $(host-generic-package))
