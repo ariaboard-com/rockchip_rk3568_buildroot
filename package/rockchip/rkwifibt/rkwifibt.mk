d2f1a614b74af6a09efaaf83416b676199c490eb
Yao Xiao <xiaoyao@rock-chips.com>
2020-06-30 11:01:08 +0800

    packages: rkwifibt: fix rtl build err without bt
    Signed-off-by: Yao Xiao <xiaoyao@rock-chips.com>
    Change-Id: Ie2fdbe40fc2d68c1454b0f5077c1698b8f8a1055

--- package/rockchip/rkwifibt/rkwifibt.mk
+++ package/rockchip/rkwifibt/rkwifibt.mk
@@ -14,7 +14,7 @@ RKWIFIBT_DEPENDENCIES = wpa_supplicant
 
 BT_TTY_DEV = $(call qstrip,$(BR2_PACKAGE_RKWIFIBT_BTUART))
 SXLOAD_WIFI = "S36load_wifi_modules"
-FIRMWARE_DIR = "system"
+FIRMWARE_DIR = system
 
 ifeq ($(call qstrip,$(BR2_ARCH)),aarch64)
 RKARCH=arm64
@@ -23,7 +23,7 @@ RKARCH=arm
 endif
 
 ifeq (y,$(BR2_TOOLCHAIN_EXTERNAL_HEADERS_4_19))
-FIRMWARE_DIR = "vendor"
+FIRMWARE_DIR = vendor
 SXLOAD_WIFI = "S36load_rv1109_wifi_modules"
 RK_WIFI_CHIP_NAME1 = AP6256
 RK_WIFI_CHIP_NAME2 = AP6255
@@ -59,9 +59,12 @@ define RKWIFIBT_BROADCOM_INSTALL
     $(INSTALL) -D -m 0755 $(TARGET_DIR)/usr/bin/bt_load_broadcom_firmware $(TARGET_DIR)/usr/bin/bt_init.sh
 endef
 
-define RKWIFIBT_REALTEK_INSTALL
+define RKWIFIBT_REALTEK_WIFI_INSTALL
     $(INSTALL) -D -m 0755 $(@D)/bin/$(RKARCH)/rtwpriv $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/$(SXLOAD_WIFI) $(TARGET_DIR)/etc/init.d/
+endef
+
+define RKWIFIBT_REALTEK_BT_INSTALL
     $(INSTALL) -D -m 0755 $(@D)/realtek/rtk_hciattach/rtk_hciattach $(TARGET_DIR)/usr/bin/rtk_hciattach
     $(INSTALL) -D -m 0755 $(@D)/bin/$(RKARCH)/* $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0644 $(@D)/realtek/$(BR2_PACKAGE_RKWIFIBT_CHIPNAME)/* $(TARGET_DIR)/lib/firmware/rtlbt/
@@ -107,10 +110,20 @@ endef
 endif
 
 ifeq ($(BR2_PACKAGE_RKWIFIBT_VENDOR), "REALTEK")
+
+ifeq ($(BR2_PACKAGE_RKWIFIBT_BT_EN), "ENABLE")
 define RKWIFIBT_INSTALL_TARGET_CMDS
     $(RKWIFIBT_INSTALL_COMMON)
-    $(RKWIFIBT_REALTEK_INSTALL)
+    $(RKWIFIBT_REALTEK_WIFI_INSTALL)
+    $(RKWIFIBT_REALTEK_BT_INSTALL)
 endef
+else
+define RKWIFIBT_INSTALL_TARGET_CMDS
+    $(RKWIFIBT_INSTALL_COMMON)
+    $(RKWIFIBT_REALTEK_WIFI_INSTALL)
+endef
+endif
+
 endif
 
 ifeq ($(BR2_PACKAGE_RKWIFIBT_VENDOR), "ROCKCHIP")
