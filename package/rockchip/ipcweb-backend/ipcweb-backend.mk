53542c3b893e443dcaa54a8fa771b1d4efea2713
allen.chen <allen.chen@rock-chips.com>
2020-08-07 16:10:35 +0800

    package: rm www before cp in ipcweb-backend
    Signed-off-by: allen.chen <allen.chen@rock-chips.com>
    Change-Id: I414fe9ab383428c206be6f2ff9470b5d5ab73afe

--- package/rockchip/ipcweb-backend/ipcweb-backend.mk
+++ package/rockchip/ipcweb-backend/ipcweb-backend.mk
@@ -11,12 +11,14 @@ IPCWEB_BACKEND_INSTALL_TARGET_OPTS = DESTDIR=$(BR2_PACKAGE_RK_OEM_INSTALL_TARGET
 IPCWEB_BACKEND_DEPENDENCIES += rk_oem
 IPCWEB_BACKEND_CONF_OPTS += -DIPCWEBBACKEND_INSTALL_ON_OEM_PARTITION=ON
 define IPCWEB_BACKEND_INSTALL_TARGET_CMDS
+	rm -rf $(BASE_DIR)/oem/www
 	cp -rfp $(@D)/www $(BASE_DIR)/oem
 	mkdir -p $(BASE_DIR)/oem/www/cgi-bin/
 	cp -rfp $(@D)/src/entry.cgi $(BASE_DIR)/oem/www/cgi-bin/
 endef
 else
 define IPCWEB_BACKEND_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/usr/www
 	cp -rfp $(@D)/www $(TARGET_DIR)/usr
 	mkdir -p  $(TARGET_DIR)/usr/www/cgi-bin/
 	cp -rfp $(@D)/src/entry.cgi $(TARGET_DIR)/usr/www/cgi-bin/
