41dffc5c13cd5eb8eecfc053c308b65cca5d26c3
Mark Huang <mark.huang@rock-chips.com>
2020-08-19 14:45:35 +0800

    package: rockit: add aiq config
    Change-Id: I23e716e6612fa535a56f81c47f24ca19fb78393a
    Signed-off-by: Mark Huang <mark.huang@rock-chips.com>

--- package/rockchip/rockit/rockit.mk
+++ package/rockchip/rockit/rockit.mk
@@ -11,8 +11,12 @@ ROCKIT_SITE_METHOD = local
 ROCKIT_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_FFMPEG),y)
-RKMEDIA_DEPENDENCIES += ffmpeg
+ROCKIT_DEPENDENCIES += ffmpeg
 endif
 
+ifeq ($(BR2_PACKAGE_CAMERA_ENGINE_RKAIQ), y)
+ROCKIT_DEPENDENCIES += camera_engine_rkaiq
+ROCKIT_CONF_OPTS += -DUSE_RKAIQ=ON
+endif
 
 $(eval $(cmake-package))
