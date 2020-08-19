9ac1f2a313f49d9dcb49c35a863810c5ae5fcda5
Mark Huang <mark.huang@rock-chips.com>
2020-08-19 17:26:06 +0800

    package: uac_app: add rockit
    Change-Id: I40aeace3844f3e96439fc8f1fcc29cbeb79195bb
    Signed-off-by: Mark Huang <mark.huang@rock-chips.com>

--- package/rockchip/uac_app/uac_app.mk
+++ package/rockchip/uac_app/uac_app.mk
@@ -2,7 +2,7 @@ ifeq ($(BR2_PACKAGE_UAC_APP), y)
     UAC_APP_SITE = $(TOPDIR)/../external/uac_app
     UAC_APP_SITE_METHOD = local
     UAC_APP_INSTALL_STAGING = YES
-    UAC_APP_DEPENDENCIES = librockit
+    UAC_APP_DEPENDENCIES = rockit
     $(eval $(cmake-package))
 endif
 
