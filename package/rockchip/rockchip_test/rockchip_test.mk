c1e1a8070432dca26e82d52a58120387435f0035
Caesar Wang <wxt@rock-chips.com>
2020-09-16 17:05:03 +0800

    package/rockchip: support rknn_demo tests
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: Iebbdcaacd2c4934ac98b579ceb5f6ea3c8bdc44a

--- package/rockchip/rockchip_test/rockchip_test.mk
+++ package/rockchip/rockchip_test/rockchip_test.mk
@@ -1,13 +1,13 @@
 # add test tool for rockchip platform
 # Author : Hans Yang <yhx@rock-chips.com>
 
-ROCKCHIP_TEST_VERSION = 20200805
+ROCKCHIP_TEST_VERSION = 20200916
 ROCKCHIP_TEST_SITE_METHOD = local
 ROCKCHIP_TEST_SITE = $(TOPDIR)/package/rockchip/rockchip_test/src
 ROCKCHIP_TEST_LICENSE = Apache V2.0
 ROCKCHIP_TEST_LICENSE_FILES = NOTICE
 
-ifeq ($(BR2_PACKAGE_RKNPU),y)
+ifeq ($(BR2_PACKAGE_RKNPU)$(BR2_PACKAGE_RKNN_DEMO),y)
 define ROCKCHIP_TEST_INSTALL_NPU_TARGET_CMDS
 	rm -rf ${TARGET_DIR}/rockchip_test/npu
 	cp -rf $(@D)/npu_${ARCH}  ${TARGET_DIR}/rockchip_test/npu
