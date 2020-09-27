00a920fa7a88c651019fe9084b83b672d96c456f
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-09-27 17:56:30 +0800

    libmali: Install non-ICD OpenCL implementation only
    The buildroot doesn't support OpenCL ICD loader yet.
    
    Change-Id: I2322e85b09ea98fae5126be046e346083bc40f70
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/libmali/libmali.mk
+++ package/rockchip/libmali/libmali.mk
@@ -12,6 +12,8 @@ LIBMALI_INSTALL_STAGING = YES
 
 LIBMALI_DEPENDENCIES = mesa3d
 
+LIBMALI_CONF_OPTS = -Dwith-overlay=true -Dopencl-icd=false
+
 ifeq ($(BR2_PACKAGE_LIBMALI_ONLY_CL),y)
 LIBMALI_CONF_OPTS += -Dplatform=only-cl
 else ifeq ($(BR2_PACKAGE_WAYLAND),y)
@@ -30,12 +32,12 @@ ifneq ($(BR2_PACKAGE_RK3326)$(BR2_PACKAGE_PX30),)
 LIBMALI_CONF_OPTS += -Dgpu=bifrost-g31 -Dversion=rxp0
 else ifeq ($(BR2_PACKAGE_PX3SE),y)
 LIBMALI_CONF_OPTS += -Dgpu=utgard-400 -Dversion=r7p0 \
-		     -Dsubversion=r3p0 -Dwith-overlay=true
+		     -Dsubversion=r3p0
 else ifneq ($(BR2_PACKAGE_RK312X)$(BR2_PACKAGE_RK3128H)$(BR2_PACKAGE_RK3036)$(BR2_PACKAGE_RK3032),)
 LIBMALI_CONF_OPTS += -Dgpu=utgard-400 -Dversion=r7p0 -Dsubversion=r1p0
 else ifeq ($(BR2_PACKAGE_RK3288),y)
 LIBMALI_CONF_OPTS += -Dgpu=midgard-t76x -Dversion=r18p0 \
-		     -Dsubversion=all -Dwith-overlay=true
+		     -Dsubversion=all
 else ifneq ($(BR2_PACKAGE_RK3399)$(BR2_PACKAGE_RK3399PRO),)
 LIBMALI_CONF_OPTS += -Dgpu=midgard-t86x -Dversion=r18p0
 else ifeq ($(BR2_PACKAGE_RK3328),y)
