bf2d47f5dc89ef6ec7e662168291c6694dc0b86a
Ziyuan Xu <xzy.xu@rock-chips.com>
2020-10-09 19:20:53 +0800

    package: thunderboot: add optional configuration for driver modules
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>
    Change-Id: I98165fd13ec75b84e38f528294d1d0873808e183

--- package/rockchip/thunderboot/thunderboot.mk
+++ package/rockchip/thunderboot/thunderboot.mk
@@ -9,6 +9,7 @@ THUNDERBOOT_SITE_METHOD = local
 THUNDERBOOT_SITE = $(TOPDIR)/package/rockchip/thunderboot
 
 KERNEL_VERSION=`make -C $(TOPDIR)/../kernel kernelversion |grep -v make`
+INSTALL_MODULES = $(call qstrip,$(BR2_THUNDERBOOT_INSTALL_MODULES))
 
 define THUNDERBOOT_BUILD_CMDS
 	make -C $(TOPDIR)/../kernel INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=${THUNDERBOOT_BUILDDIR} modules_install ARCH=${BR2_ARCH}
@@ -16,7 +17,11 @@ endef
 
 define THUNDERBOOT_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/modules/ $(TARGET_DIR)/etc/preinit.d/
-	$(INSTALL) -D -m 644 ${THUNDERBOOT_BUILDDIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/mmc/host/dw_mmc-rockchip.ko $(TARGET_DIR)/lib/modules/
+
+	for module in `echo ${INSTALL_MODULES} | tr ',' '\n'`; do \
+		find ${THUNDERBOOT_BUILDDIR}/lib/modules/${KERNEL_VERSION}/kernel -name $$module | xargs -i cp {} $(TARGET_DIR)/lib/modules/; \
+	done
+
 	$(INSTALL) -D -m 755 $(@D)/S07mountall $(TARGET_DIR)/etc/preinit.d/
 	$(INSTALL) -D -m 755 $(@D)/tb_poweroff $(TARGET_DIR)/usr/bin/
 endef
