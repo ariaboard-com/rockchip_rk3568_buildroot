3bd414e8b9ad69f72a6339ca5d5b45a619430456
Mark Huang <huangjc@rock-chips.com>
2020-11-02 13:52:31 +0800

    board: rv1126_rv1109: uvcc: mv post-build-uvcc.sh to pack-uvc-oem.sh
    Change-Id: I9b4e6921b7c3b58d65a00ecc5b600b12b1f4ebe5
    Signed-off-by: Mark Huang <huangjc@rock-chips.com>

--- board/rockchip/rv1126_rv1109/pack-uvc-oem.sh
+++ board/rockchip/rv1126_rv1109/pack-uvc-oem.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-BUILDROOT=$(pwd)
-TARGET=$1
-NAME=$(whoami)
-HOST=$(hostname)
-KERNEL_DIR=$BUILDROOT/../kernel
-OUTPUT_DIR=$TARGET/../
+BUILDROOT_DIR=`pwd`
+SDK_DIR=`dirname $BUILDROOT_DIR`
 
-
-rm -rf $TARGET/usr/lib/libstdc++.so*
 echo "==========build oem to rootfs=========="
 echo "$BUILDROOT_DIR"
 echo "$SDK_DIR"
 echo "$RK_TARGET_PRODUCT"
 echo "$RK_CFG_BUILDROOT"
+
+rm -rf $SDK_DIR/buildroot/output/$RK_CFG_BUILDROOT/target/usr/lib/libstdc++.so*
 
 rm -rf $SDK_DIR/buildroot/output/$RK_CFG_BUILDROOT/target/oem/
 cp -rf $SDK_DIR/buildroot/output/$RK_CFG_BUILDROOT/oem/. $SDK_DIR/buildroot/output/$RK_CFG_BUILDROOT/target/oem/
