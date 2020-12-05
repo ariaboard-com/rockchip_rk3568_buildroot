383879ffdadfcb92efafcb84c331888d5b4ab2d6
Zhihua Wang <hogan.wang@rock-chips.com>
2020-12-05 16:57:27 +0800

    package: rockchip: uvc_app: remove rkmedia dependency
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: Iedd60cb17907953fad965f61bb7e80e3bd82adac

--- package/rockchip/uvc_app/uvc_app.mk
+++ package/rockchip/uvc_app/uvc_app.mk
@@ -7,13 +7,6 @@ ifeq ($(BR2_PACKAGE_CAMERA_ENGINE_RKISP),y)
     UVC_APP_DEPENDENCIES += camera_engine_rkisp
 endif
 
-ifneq ($(BR2_PACKAGE_RKMEDIA_UVC), y)
-ifeq ($(BR2_PACKAGE_RKMEDIA), y)
-    UVC_APP_DEPENDENCIES += rkmedia
-    UVC_APP_CONF_OPTS = -DCOMPILES_CAMERA=ON
-endif
-endif
-
 ifeq ($(BR2_PACKAGE_AISERVER), y)
     UVC_APP_CONF_OPTS += -DCOMPILES_CAMERA=ON -DENABLE_AISERVER=ON
 endif
@@ -31,6 +24,10 @@ endif
 ifeq ($(BR2_PACKAGE_DBSERVER),y)
     UVC_APP_DEPENDENCIES += libIPCProtocol
     UVC_APP_CONF_OPTS += "-DDBSERVER_SUPPORT=ON" "-DLIBIPCPROTOCOL_HEADER_DIR=$(STAGING_DIR)/usr/include/libIPCProtocol"
+endif
+
+ifeq ($(BR2_PACKAGE_RKMEDIA), y)
+    UVC_APP_CONF_OPTS = -DINSTALL_LIBRKUVC=y
 endif
 
     $(eval $(cmake-package))
