ariaboard-com/rockchip_rk3568_buildroot

commit d390acd776024c8f1b6bc275d8d8c4a7902a927d

buildroot: rkwifibt: support cyw chips lowpower mode
Signed-off-by: Yao Xiao <xiaoyao@rock-chips.com>
Change-Id: Ie03ea2065e5ff18f2bf2fec3d2826bb7a9ce6c60

package/rockchip/rkwifibt/rkwifibt.mk
@@ -50,11 +50,13 @@ define RKWIFIBT_INSTALL_COMMON
 endef
 
 define RKWIFIBT_TB_INSTALL
-	mkdir -p $(TARGET_DIR)/$(FIRMWARE_DIR)/etc/firmware
+    mkdir -p $(TARGET_DIR)/$(FIRMWARE_DIR)/etc/firmware
     $(INSTALL) -D -m 0755 $(@D)/wpa_supplicant.conf $(TARGET_DIR)/etc/
     $(INSTALL) -D -m 0644 $(@D)/firmware/broadcom/$(BR2_PACKAGE_RKWIFIBT_CHIPNAME)/wifi/* $(TARGET_DIR)/$(FIRMWARE_DIR)/etc/firmware/
     $(INSTALL) -D -m 0755 $(@D)/tb_start_wifi.sh $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -D -m 0755 $(@D)/tb_start_cy_wifi.sh $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/brcm_tools/dhd_priv $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -D -m 0755 $(@D)/src/CY_WL_API/keepalive $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/bin/$(RKARCH)/* $(TARGET_DIR)/usr/bin/
 
     $(INSTALL) -D -m 0644 $(TOPDIR)/../kernel/drivers/net/wireless/rockchip_wlan/rkwifi/rk_wifi_config.ko $(TARGET_DIR)/$(FIRMWARE_DIR)/lib/modules/
@@ -113,6 +115,7 @@ define RKWIFIBT_BUILD_CMDS
     $(TARGET_CC) -o $(@D)/brcm_tools/dhd_priv $(@D)/brcm_tools/dhd_priv.c
     $(TARGET_CC) -o $(@D)/src/rk_wifi_init $(@D)/src/rk_wifi_init.c
     $(MAKE) -C $(@D)/realtek/rtk_hciattach/ CC=$(TARGET_CC)
+    $(MAKE) -C $(@D)/src/CY_WL_API/ CC=$(TARGET_CC)
     $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(TOPDIR)/../kernel/ M=$(@D)/realtek/bluetooth_uart_driver ARCH=$(RKARCH)
 endef
 
@@ -125,22 +128,20 @@ define RKWIFIBT_INSTALL_TARGET_CMDS
 endef
 endif
 
-else
-
-ifeq ($(BR2_PACKAGE_RKWIFIBT_VENDOR), "BROADCOM")
-define RKWIFIBT_INSTALL_TARGET_CMDS
-    $(RKWIFIBT_TB_INSTALL)
-endef
-endif
-
-endif #THUNDERBOOT
-
 ifeq ($(BR2_PACKAGE_RKWIFIBT_VENDOR), "CYPRESS")
 define RKWIFIBT_INSTALL_TARGET_CMDS
     $(RKWIFIBT_INSTALL_COMMON)
     $(RKWIFIBT_BROADCOM_INSTALL)
 endef
 endif
+
+else
+
+define RKWIFIBT_INSTALL_TARGET_CMDS
+    $(RKWIFIBT_TB_INSTALL)
+endef
+
+endif #THUNDERBOOT
 
 ifeq ($(BR2_PACKAGE_RKWIFIBT_VENDOR), "REALTEK")
 

package/rockchip/rkwifibt/Config.in
@@ -31,7 +31,7 @@ config BR2_PACKAGE_RKWIFIBT_AMPAKALL
 config BR2_PACKAGE_RKWIFIBT_AWCM256
     bool "AW-CM256"
 config BR2_PACKAGE_RKWIFIBT_AWNB197
-    bool "AW-NAB197"
+    bool "AW-NB197"
 config BR2_PACKAGE_RKWIFIBT_RTL8188EU
     bool "RTL8188EU"
 config BR2_PACKAGE_RKWIFIBT_RTL8188FU
@@ -85,7 +85,7 @@ config BR2_PACKAGE_RKWIFIBT_CHIPNAME
     default "AP6203BM" if BR2_PACKAGE_RKWIFIBT_AP6203BM
     default "all" if BR2_PACKAGE_RKWIFIBT_AMPAKALL
     default "AW-CM256" if BR2_PACKAGE_RKWIFIBT_AWCM256
-    default "AW-NAB197" if BR2_PACKAGE_RKWIFIBT_AWNB197
+    default "AW-NB197" if BR2_PACKAGE_RKWIFIBT_AWNB197
     default "RTL8188EU" if BR2_PACKAGE_RKWIFIBT_RTL8188EU
     default "RTL8188FU" if BR2_PACKAGE_RKWIFIBT_RTL8188FU
     default "RTL8189ES" if BR2_PACKAGE_RKWIFIBT_RTL8189ES

commit a508a375fc0b9d406ddb826fa25ae781910edb95

package: rkwifibt: remove tb_start_cy_wifi.sh install
Fixes: d390acd77602 ("buildroot: rkwifibt: support cyw chips lowpower mode")
Change-Id: I2ddfd91ea23ed0547f4644f9978299dc645ee01c
Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

package/rockchip/rkwifibt/rkwifibt.mk
@@ -54,7 +54,6 @@ define RKWIFIBT_TB_INSTALL
     $(INSTALL) -D -m 0755 $(@D)/wpa_supplicant.conf $(TARGET_DIR)/etc/
     $(INSTALL) -D -m 0644 $(@D)/firmware/broadcom/$(BR2_PACKAGE_RKWIFIBT_CHIPNAME)/wifi/* $(TARGET_DIR)/$(FIRMWARE_DIR)/etc/firmware/
     $(INSTALL) -D -m 0755 $(@D)/tb_start_wifi.sh $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -D -m 0755 $(@D)/tb_start_cy_wifi.sh $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/brcm_tools/dhd_priv $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/src/CY_WL_API/keepalive $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/bin/$(RKARCH)/* $(TARGET_DIR)/usr/bin/