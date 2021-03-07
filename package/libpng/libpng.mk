b5b855b855a70c34f6f31cffccb8c5bd42f0918d
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-03-08 03:15:40 +0800

    libpng: Bump to buildroot upstream 1.6.37 and support libpng12
    Bump to:
    7abc0d1001 Kickoff 2021.05 cycle
    a3a9d17e43 docs/website/news.html: add 2021.02 announcement link
    e2fba6457b Update for 2021.02
    1e4bc89960 package/quagga: fix build with gcc 10
    238b5df775 package/wolfssl: security bump to version 4.7.0
    308f4428c8 package/libjpeg: fix LIBJPEG_SITE
    
    And add libpng 1.2.59.
    
    Change-Id: I59402efed3093ff8d516d8e3513a8eae048866a8
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,18 +4,25 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.6.34
-LIBPNG_SERIES = 16
-LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
-LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng${LIBPNG_SERIES}/$(LIBPNG_VERSION)
+ifeq ($(BR2_PACKAGE_LIBPNG_12),y)
+LIBPNG_VERSION = 1.2.59
+LIBPNG_SERIES = 12
 LIBPNG_LICENSE = Libpng
+else
+LIBPNG_VERSION = 1.6.37
+LIBPNG_SERIES = 16
+LIBPNG_LICENSE = Libpng-2.0
+endif
+
+LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
+LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng$(LIBPNG_SERIES)/$(LIBPNG_VERSION)
 LIBPNG_LICENSE_FILES = LICENSE
 LIBPNG_INSTALL_STAGING = YES
 LIBPNG_DEPENDENCIES = host-pkgconf zlib
 HOST_LIBPNG_DEPENDENCIES = host-pkgconf host-zlib
 LIBPNG_CONFIG_SCRIPTS = libpng$(LIBPNG_SERIES)-config libpng-config
 
-ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+ifeq ($(BR2_ARM_CPU_HAS_NEON)$(BR2_aarch64),y)
 LIBPNG_CONF_OPTS += --enable-arm-neon
 else
 LIBPNG_CONF_OPTS += --disable-arm-neon
