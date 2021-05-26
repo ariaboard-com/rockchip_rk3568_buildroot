0bc086402f2ffdd439d5ad5e04357d64cc80997c
liuth <lth@t-tchip.com.cn>
2021-05-26 17:17:52 +0800

    broadcom_bsa: auto get bt_tty_dev and fix bug

--- package/rockchip/broadcom_bsa/broadcom_bsa.mk
+++ package/rockchip/broadcom_bsa/broadcom_bsa.mk
@@ -16,7 +16,7 @@ ifeq (y,$(BR2_TOOLCHAIN_EXTERNAL_HEADERS_4_19))
 endif
 
 ifeq (y,$(BR2_KERNEL_HEADERS_4_19))
-	FIRMWARE_DIR = vendor
+	BT_FW_DIR = vendor
 endif
 
 ifeq ($(BR2_PACKAGE_BROADCOM_BSA)$(BR2_PACKAGE_CYPRESS_BSA),yy)
@@ -73,10 +73,15 @@ define BROADCOM_BSA_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(TOPDIR)/../external/broadcom_bsa/$(BROADCOM_BSA_BLE_WIFI_CONFIG_FILE) $(TARGET_DIR)/usr/bin/bsa_ble_wifi_introducer.sh
 	sed -i 's/BTFIRMWARE_PATH/\/$(BT_FW_DIR)\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_bt_hfp.sh
 	sed -i 's/BTFIRMWARE_PATH/\/$(BT_FW_DIR)\/etc\/firmware\//g' $(TARGET_DIR)/usr/bin/bsa_server.sh
-	sed -i 's/BT_TTY_DEV/\/dev\/$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_server.sh
 	sed -i 's/BTFIRMWARE_PATH/\/$(BT_FW_DIR)\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_bt_sink.sh
 	sed -i 's/BTFIRMWARE_PATH/\/$(BT_FW_DIR)\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_bt_source.sh
 	sed -i 's/BTFIRMWARE_PATH/\/$(BT_FW_DIR)\/etc\/firmware\/$(BTFIRMWARE)/g' $(TARGET_DIR)/usr/bin/bsa_ble_wifi_introducer.sh
+
+	sed -i 's/BT_TTY_DEV/\/dev\/$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_server.sh
+	sed -i 's/ttyS./$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_bt_source.sh
+	sed -i 's/ttyS./$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_bt_sink.sh
+	sed -i 's/ttyS./$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_bt_hfp.sh
+	sed -i 's/ttyS./$(BT_TTY_DEV)/g' $(TARGET_DIR)/usr/bin/bsa_ble_wifi_introducer.sh
 
 endef
 
